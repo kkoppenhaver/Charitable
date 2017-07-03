fc9dbb51ceb69993865afda4bd8bffcd15975ae1
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:52:14 +0100

    Code consistency changes for tests/helpers/charitable-donor-helper.php

--- tests/helpers/charitable-donor-helper.php
+++ tests/helpers/charitable-donor-helper.php
@@ -7,9 +7,9 @@
 class Charitable_Donor_Helper extends WP_UnitTestCase {
 
 	/**
-	 * Delete a donor 
+	 * Delete a donor
 	 *
-	 * @return 	void
+	 * @return void
 	 * @access  public
 	 * @since 	1.0.0
 	 */
@@ -20,27 +20,26 @@ class Charitable_Donor_Helper extends WP_UnitTestCase {
 	/**
 	 * Create a donor.
 	 *
-	 * @param 	array 		$args 				Optional arguments.
-	 * @return 	int 		$donor_id
+	 * @param  array $args     Optional arguments.
+	 * @return int   $donor_id
 	 * @access  public
 	 * @static
-	 * @since 	1.0.0	 
+	 * @since 	1.0.0
 	 */
 	public static function create_donor( $args = array() ) {
-
 		$defaults = array(
-			'user_email'	=> 'johndoe@example.com', 
-			'user_login'	=> 'johndoe',
-			'first_name'	=> 'John',	
-			'last_name'		=> 'Doe',
-			'address'		=> 'Unit A',
-			'address_2'		=> '164 Studio Street',		
-			'city'			=> 'Melbourne',
-			'state'			=> 'VIC',		
-			'postcode'		=> '3000',
-			'country'		=> 'Australia',
-			'phone'			=> '0390009000'
-		);	
+			'user_email' => 'johndoe@example.com',
+			'user_login' => 'johndoe',
+			'first_name' => 'John',
+			'last_name'  => 'Doe',
+			'address'    => 'Unit A',
+			'address_2'  => '164 Studio Street',
+			'city'       => 'Melbourne',
+			'state'      => 'VIC',
+			'postcode'   => '3000',
+			'country'    => 'Australia',
+			'phone'      => '0390009000'
+		);
 
 		$args = array_merge( $defaults, $args );
 
@@ -50,20 +49,21 @@ class Charitable_Donor_Helper extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Create a donor with a name.  
+	 * Create a donor with a name.
 	 *
-	 * @param 	string 		$first_name
-	 * @param 	string 		$last_name
-	 * @param 	array 		$args
-	 * @return 	int
+	 * @param  string $first_name
+	 * @param  string $last_name
+	 * @param  array  $args
+	 * @return int
 	 * @access  public
 	 * @static
 	 * @since 	1.0.0
 	 */
 	public static function create_named_donor( $first_name, $last_name, $args = array() ) {
 		$args['first_name'] = $first_name;
-		$args['last_name'] = $last_name;
+		$args['last_name']  = $last_name;
 		$args['user_email'] = $first_name . $last_name . '@example.com';
 		return self::create_donor( $args );
 	}
+
 }
