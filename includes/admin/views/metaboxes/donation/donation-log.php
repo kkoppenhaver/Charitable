d84395515f161bdb2a4f204c09cf650ea9f7dadf
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:44:39 +0100

    Code consistency changes for includes/admin/views/metaboxes/donation/donation-log.php

--- includes/admin/views/metaboxes/donation/donation-log.php
+++ includes/admin/views/metaboxes/donation/donation-log.php
@@ -9,23 +9,23 @@ global $post;
 
 $logs = get_post_meta( $post->ID, '_donation_log', true );
 
-$date_format = get_option( 'date_format' );
-$time_format = get_option( 'time_format' );
+$date_format      = get_option( 'date_format' );
+$time_format      = get_option( 'time_format' );
 $date_time_format = "$date_format - $time_format";
 ?>
 <div id="charitable-donation-log-metabox" class="charitable-metabox">
 	<table class="widefat">
 		<thead>
 			<tr>
-				<th><?php _e( 'Date &amp; Time', 'charitable' ) ?></th>
-				<th><?php _e( 'Log', 'charitable' ) ?></th>
+				<th><?php _e( 'Date &amp; Time', 'charitable' ); ?></th>
+				<th><?php _e( 'Log', 'charitable' ); ?></th>
 			</th>
 		</thead>
 		<?php foreach ( $logs as $log ) : ?>
 		<tr>
-			<td><?php echo date_i18n( $date_time_format, $log['time'] ) ?></td>
-			<td><?php echo $log['message'] ?></td>
+			<td><?php echo date_i18n( $date_time_format, $log['time'] ); ?></td>
+			<td><?php echo $log['message']; ?></td>
 		</tr>
-		<?php endforeach ?>
+		<?php endforeach; ?>
 	</table>
 </div>
