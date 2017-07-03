0269018e745722f383aec25d1fc514ef95d73377
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:45:18 +0100

    Code consistency changes for includes/admin/views/settings/multi-checkbox.php

--- includes/admin/views/settings/multi-checkbox.php
+++ includes/admin/views/settings/multi-checkbox.php
@@ -10,52 +10,52 @@
 $value = charitable_get_option( $view_args['key'], array() );
 
 if ( empty( $value ) ) :
-    $value = isset( $view_args['default'] ) ? (array) $view_args['default'] : array();
+	$value = isset( $view_args['default'] ) ? (array) $view_args['default'] : array();
 endif;
 
 if ( ! is_array( $value ) ) {
-    $value = (array) $value;
+	$value = (array) $value;
 }
 
 $setting_attributes = '';
 
 if ( array_key_exists( 'attrs', $view_args ) ) {
 
-    $setting_attributes_keys = array( 'data-trigger-key', 'data-trigger-change-type', 'data-trigger-value' );
+	$setting_attributes_keys = array( 'data-trigger-key', 'data-trigger-change-type', 'data-trigger-value' );
 
-    foreach ( $setting_attributes_keys as $key ) {
+	foreach ( $setting_attributes_keys as $key ) {
 
-        if ( ! array_key_exists( $key, $view_args['attrs'] ) ) {
-            continue;
-        }
+		if ( ! array_key_exists( $key, $view_args['attrs'] ) ) {
+			continue;
+		}
 
-        $setting_attributes .= sprintf( ' %s="%s"', $key, esc_attr( $view_args['attrs'][ $key ] ) );
+		$setting_attributes .= sprintf( ' %s="%s"', $key, esc_attr( $view_args['attrs'][ $key ] ) );
 
-        unset( $view_args['attrs'][ $key ] );        
+		unset( $view_args['attrs'][ $key ] );
 
-    }
+	}
 }
 
 ?>
-<ul class="charitable-checkbox-list <?php echo esc_attr( $view_args[ 'classes' ] ) ?>" <?php echo $setting_attributes ?>>
+<ul class="charitable-checkbox-list <?php echo esc_attr( $view_args[ 'classes' ] ); ?>" <?php echo $setting_attributes; ?>>
 
-    <?php foreach ( $view_args[ 'options' ] as $option => $label ) : ?>
+    <?php foreach ( $view_args['options'] as $option => $label ) : ?>
 
-        <li><input type="checkbox" 
-                id="<?php printf( 'charitable_settings_%s_%s', implode( '_', $view_args[ 'key' ] ), $option ) ?>" 
-                name="<?php printf( 'charitable_settings[%s][]', $view_args[ 'name' ] ) ?>"
-                value="<?php echo esc_attr( $option ) ?>"
-                <?php checked( in_array( $option, $value ) ) ?> 
-                <?php echo charitable_get_arbitrary_attributes( $view_args ) ?>
+        <li><input type="checkbox"
+                id="<?php printf( 'charitable_settings_%s_%s', implode( '_', $view_args[ 'key' ] ), $option ); ?>"
+                name="<?php printf( 'charitable_settings[%s][]', $view_args[ 'name' ] ); ?>"
+                value="<?php echo esc_attr( $option ); ?>"
+                <?php checked( in_array( $option, $value ) ); ?>
+                <?php echo charitable_get_arbitrary_attributes( $view_args ); ?>
                 />
-            <?php echo $label ?>
+            <?php echo $label; ?>
         </li>
 
-    <?php endforeach ?>
+    <?php endforeach; ?>
 
 </ul>
 <?php if ( isset( $view_args['help'] ) ) : ?>
 
-    <div class="charitable-help"><?php echo $view_args['help']  ?></div>
+    <div class="charitable-help"><?php echo $view_args['help'];  ?></div>
 
 <?php endif;
