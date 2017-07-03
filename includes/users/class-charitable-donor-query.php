a70bdc337ae5fc9c3288db0e1caa1c7ba8327069
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:49:15 +0100

    Code consistency changes for includes/users/class-charitable-donor-query.php

--- includes/users/class-charitable-donor-query.php
+++ includes/users/class-charitable-donor-query.php
@@ -24,7 +24,7 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 		/**
 		 * Create new query object.
 		 *
-		 * @param   array $args Query arguments.
+		 * @param array $args Query arguments.
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -52,7 +52,7 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 		/**
 		 * Return list of donor IDs together with the number of donations they have made.
 		 *
-		 * @return  object[]
+		 * @return object[]
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -68,7 +68,7 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 			foreach ( $records as $row ) {
 
 				$donation_id = isset( $row->donation_id ) ? $row->donation_id : false;
-				$objects[] = new Charitable_Donor( $row->donor_id, $donation_id );
+				$objects[]   = new Charitable_Donor( $row->donor_id, $donation_id );
 
 			}
 
@@ -78,7 +78,7 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 		/**
 		 * Set up fields query argument.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -102,7 +102,7 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 		/**
 		 * Set up orderby query argument.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -131,7 +131,7 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 		/**
 		 * Set up query grouping.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -148,48 +148,49 @@ if ( ! class_exists( 'Charitable_Donor_Query' ) ) :
 		/**
 		 * Remove any hooks that have been attached by the class to prevent contaminating other queries.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
 		public function unhook_callbacks() {
-			remove_action( 'charitable_pre_query',     array( $this, 'setup_fields' ) );
-			remove_action( 'charitable_pre_query',     array( $this, 'setup_orderby' ) );
-			remove_action( 'charitable_pre_query',     array( $this, 'setup_grouping' ) );
-			remove_filter( 'charitable_query_fields',  array( $this, 'donation_fields' ), 4 );
-			remove_filter( 'charitable_query_fields',  array( $this, 'donation_calc_fields' ), 5 );
-			remove_filter( 'charitable_query_fields',  array( $this, 'donor_fields' ), 6 );
-			remove_filter( 'charitable_query_fields',  array( $this, 'donation_amount_sum_field' ), 6 );
-			remove_filter( 'charitable_query_join',    array( $this, 'join_campaign_donations_table_on_donation' ), 5 );
-			remove_filter( 'charitable_query_join',    array( $this, 'join_donors_table' ), 6 );
-			remove_filter( 'charitable_query_where',   array( $this, 'where_status_is_in' ), 5 );
-			remove_filter( 'charitable_query_where',   array( $this, 'where_campaign_is_in' ), 6 );
-			remove_filter( 'charitable_query_where',   array( $this, 'where_donor_id_is_in' ), 7 );
+			remove_action( 'charitable_pre_query', array( $this, 'setup_fields' ) );
+			remove_action( 'charitable_pre_query', array( $this, 'setup_orderby' ) );
+			remove_action( 'charitable_pre_query', array( $this, 'setup_grouping' ) );
+			remove_filter( 'charitable_query_fields', array( $this, 'donation_fields' ), 4 );
+			remove_filter( 'charitable_query_fields', array( $this, 'donation_calc_fields' ), 5 );
+			remove_filter( 'charitable_query_fields', array( $this, 'donor_fields' ), 6 );
+			remove_filter( 'charitable_query_fields', array( $this, 'donation_amount_sum_field' ), 6 );
+			remove_filter( 'charitable_query_join', array( $this, 'join_campaign_donations_table_on_donation' ), 5 );
+			remove_filter( 'charitable_query_join', array( $this, 'join_donors_table' ), 6 );
+			remove_filter( 'charitable_query_where', array( $this, 'where_status_is_in' ), 5 );
+			remove_filter( 'charitable_query_where', array( $this, 'where_campaign_is_in' ), 6 );
+			remove_filter( 'charitable_query_where', array( $this, 'where_donor_id_is_in' ), 7 );
 			remove_filter( 'charitable_query_groupby', array( $this, 'groupby_donor_id' ) );
 			remove_filter( 'charitable_query_orderby', array( $this, 'orderby_date' ) );
 			remove_filter( 'charitable_query_orderby', array( $this, 'orderby_count' ) );
 			remove_filter( 'charitable_query_orderby', array( $this, 'orderby_donation_amount' ) );
-			remove_action( 'charitable_post_query',    array( $this, 'unhook_callbacks' ) );
+			remove_action( 'charitable_post_query', array( $this, 'unhook_callbacks' ) );
 		}
 
 		/**
 		 * Set up callbacks for WP_Query filters.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  protected
 		 * @since   1.0.0
 		 */
 		protected function prepare_query() {
-			add_action( 'charitable_pre_query',   array( $this, 'setup_fields' ) );
-			add_action( 'charitable_pre_query',   array( $this, 'setup_orderby' ) );
-			add_action( 'charitable_pre_query',   array( $this, 'setup_grouping' ) );
-			add_filter( 'charitable_query_join',  array( $this, 'join_campaign_donations_table_on_donation' ), 5 );
-			add_filter( 'charitable_query_join',  array( $this, 'join_donors_table' ), 6 );
+			add_action( 'charitable_pre_query', array( $this, 'setup_fields' ) );
+			add_action( 'charitable_pre_query', array( $this, 'setup_orderby' ) );
+			add_action( 'charitable_pre_query', array( $this, 'setup_grouping' ) );
+			add_filter( 'charitable_query_join', array( $this, 'join_campaign_donations_table_on_donation' ), 5 );
+			add_filter( 'charitable_query_join', array( $this, 'join_donors_table' ), 6 );
 			add_filter( 'charitable_query_where', array( $this, 'where_status_is_in' ), 5 );
 			add_filter( 'charitable_query_where', array( $this, 'where_campaign_is_in' ), 6 );
 			add_filter( 'charitable_query_where', array( $this, 'where_donor_id_is_in' ), 7 );
-			add_action( 'charitable_post_query',  array( $this, 'unhook_callbacks' ) );
+			add_action( 'charitable_post_query', array( $this, 'unhook_callbacks' ) );
 		}
+
 	}
 
 endif;
