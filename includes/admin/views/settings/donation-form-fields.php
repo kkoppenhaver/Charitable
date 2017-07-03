379936719948f7cfc8f367d51bc46205fa3c311d
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:44:59 +0100

    Code consistency changes for includes/admin/views/settings/donation-form-fields.php

--- includes/admin/views/settings/donation-form-fields.php
+++ includes/admin/views/settings/donation-form-fields.php
@@ -7,41 +7,41 @@
  * @since   1.0.0
  */
 
-if ( ! isset( $view_args[ 'form' ] ) || ! isset( $view_args[ 'field' ] ) ) {
-    return;
+if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
+	return;
 }
 
-$form           = $view_args[ 'form' ];
-$field          = $view_args[ 'field' ];
-$classes        = $view_args[ 'classes' ];
-$options        = isset( $field[ 'options' ] )      ? $field[ 'options' ]       : array();
-$value          = isset( $field[ 'value' ] )        ? (array) $field[ 'value' ] : array();
-$placeholder    = isset( $field[ 'placeholder' ] )  ? $field[ 'placeholder' ]   : '';
+$form        = $view_args['form'];
+$field       = $view_args['field'];
+$classes     = $view_args['classes'];
+$options     = isset( $field['options'] )      ? $field['options']       : array();
+$value       = isset( $field['value'] )        ? (array) $field['value'] : array();
+$placeholder = isset( $field['placeholder'] )  ? $field['placeholder']   : '';
 
 if ( empty( $options ) ) {
-    return;
+	return;
 }
 ?>
-<div id="charitable_field_<?php echo $field[ 'key' ] ?>" class="<?php echo $classes ?>">
-    
-    <?php if ( isset( $field[ 'label' ] ) ) : ?>
+<div id="charitable_field_<?php echo $field[ 'key' ]; ?>" class="<?php echo $classes; ?>">
 
-        <label for="charitable_field_<?php echo $field[ 'key' ] ?>">
-            <?php echo $field[ 'label' ] ?>         
+    <?php if ( isset( $field['label'] ) ) : ?>
+
+        <label for="charitable_field_<?php echo $field[ 'key' ]; ?>">
+            <?php echo $field['label']; ?>
         </label>
 
-    <?php endif ?>
+    <?php endif; ?>
 
     <ul class="options">
 
     <?php foreach ( $options as $val => $label ) : ?>
 
         <li>
-            <input type="checkbox" name="<?php echo $field[ 'key' ] ?>[]" value="<?php echo $val ?>" <?php checked( in_array( $val, $value ) ) ?> />
-            <?php echo $label ?>
+            <input type="checkbox" name="<?php echo $field[ 'key' ]; ?>[]" value="<?php echo $val; ?>" <?php checked( in_array( $val, $value ) ); ?> />
+            <?php echo $label; ?>
         </li>
 
-    <?php endforeach ?>
+    <?php endforeach; ?>
 
     </ul>
 </div>
