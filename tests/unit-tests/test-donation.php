76e317b0eb945ceccf55b0d22d6ef349d4862737
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:52:37 +0100

    Code consistency changes for tests/unit-tests/test-donation.php

--- tests/unit-tests/test-donation.php
+++ tests/unit-tests/test-donation.php
@@ -14,25 +14,25 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 		parent::setUp();
 
 		/* Campaign 1: $40,000 goal, 300 days till end */
-		$campaign_1_id 	= Charitable_Campaign_Helper::create_campaign( array( 
-			'post_title'					=> 'Test Campaign 1',
-			'_campaign_goal' 				=> 40000.00,
-			'_campaign_end_date' 			=> date( 'Y-m-d H:i:s', strtotime( '+300 days' ) )
+		$campaign_1_id = Charitable_Campaign_Helper::create_campaign( array(
+			'post_title'         => 'Test Campaign 1',
+			'_campaign_goal'     => 40000.00,
+			'_campaign_end_date' => date( 'Y-m-d H:i:s', strtotime( '+300 days' ) )
 		) );
 
 		$this->campaign_1 = new Charitable_Campaign( get_post( $campaign_1_id ) );
 
 		/* Campaign 2: $40,000 goal, 300 days till end */
-		$campaign_2_id 	= Charitable_Campaign_Helper::create_campaign( array( 
-			'post_title'					=> 'Test Campaign 2',
-			'_campaign_goal' 				=> 10000.00
+		$campaign_2_id = Charitable_Campaign_Helper::create_campaign( array(
+			'post_title'     => 'Test Campaign 2',
+			'_campaign_goal' => 10000.00
 		) );
 
 		$this->campaign_2 = new Charitable_Campaign( get_post( $campaign_2_id ) );
 
 		/* Create a couple donations */
-		$this->user_id = $this->factory->user->create( array( 'display_name' => 'John Henry' ) );
-		$user = new Charitable_User( $this->user_id );
+		$this->user_id  = $this->factory->user->create( array( 'display_name' => 'John Henry' ) );
+		$user           = new Charitable_User( $this->user_id );
 		$this->donor_id = $user->add_donor();
 	}
 
@@ -42,10 +42,10 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_add_donation() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
 			)
 		) );
@@ -59,10 +59,10 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_ID() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
 			)
 		) );
@@ -70,7 +70,7 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 		$donation = charitable_get_donation( $donation_id );
 
 		$this->assertEquals( $donation_id, $donation->ID );
-	}	
+	}
 
 	/**
 	 * @depends test_add_donation
@@ -78,13 +78,13 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_gateway() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'gateway' => 'stripe'
+			),
+			'gateway'   => 'stripe'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -98,10 +98,10 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_total_donation_amount() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
 			)
 		) );
@@ -118,15 +118,15 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_total_donation_amount_multi_donation() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 50
-				), 
-				array( 
-					'campaign_id' 	=> $this->campaign_2->ID,
-					'campaign_name'	=> 'Test Campaign 2', 
-					'amount'		=> 75
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 50
+				),
+				array(
+					'campaign_id'   => $this->campaign_2->ID,
+					'campaign_name' => 'Test Campaign 2',
+					'amount'        => 75
 				)
 			)
 		) );
@@ -143,10 +143,10 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_campaign_donations() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
 			)
 		) );
@@ -164,15 +164,15 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_campaign_donations_multi() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 50
-				), 
-				array( 
-					'campaign_id' 	=> $this->campaign_2->ID,
-					'campaign_name'	=> 'Test Campaign 2', 
-					'amount'		=> 75
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 50
+				),
+				array(
+					'campaign_id'   => $this->campaign_2->ID,
+					'campaign_name' => 'Test Campaign 2',
+					'amount'        => 75
 				)
 			)
 		) );
@@ -180,7 +180,7 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 		$donation = charitable_get_donation( $donation_id );
 
 		$this->assertCount( 2, $donation->get_campaign_donations() );
-	}	
+	}
 
 	/**
 	 * @depends test_add_donation
@@ -188,13 +188,13 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_notes() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'note' => 'This is a note'
+			),
+			'note'      => 'This is a note'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -208,13 +208,13 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_status() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-completed'
+			),
+			'status'    => 'charitable-completed'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -228,13 +228,13 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_donor() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-completed'
+			),
+			'status'    => 'charitable-completed'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -248,19 +248,19 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_donation_log() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-completed'
+			),
+			'status'    => 'charitable-completed'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
 
 		$this->assertCount( 1, $donation->get_donation_log() );
-	}	
+	}
 
 	/**
 	 * @depends test_add_donation
@@ -269,13 +269,13 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_update_donation_log() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-completed'
+			),
+			'status'    => 'charitable-completed'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -283,7 +283,7 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 		$donation->update_donation_log( 'New message' );
 
 		$this->assertCount( 2, $donation->get_donation_log() );
-	}		
+	}
 
 	/**
 	 * @depends test_add_donation
@@ -291,13 +291,13 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_update_status() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-pending'
+			),
+			'status'    => 'charitable-pending'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -313,20 +313,20 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_update_status_log() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-pending'
+			),
+			'status'    => 'charitable-pending'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
 
 		$donation->update_status( 'charitable-completed' );
 
-		$log = $donation->get_donation_log();
+		$log  = $donation->get_donation_log();
 		$last = array_pop( $log );
 
 		$this->assertEquals( 'Donation status updated from Pending to Paid.', $last['message'] );
@@ -335,18 +335,18 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_campaigns() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign 1', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign 1',
+					'amount'        => 10
 				),
-				array( 
-					'campaign_id' 	=> $this->campaign_2->ID,
-					'campaign_name'	=> 'Test Campaign 2', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_2->ID,
+					'campaign_name' => 'Test Campaign 2',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-pending'
+			),
+			'status'    => 'charitable-pending'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -357,18 +357,18 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	public function test_get_campaigns_donated_to() {
 		$donation_id = Charitable_Donation_Helper::create_donation( array(
 			'campaigns' => array(
-				array( 
-					'campaign_id' 	=> $this->campaign_1->ID,
-					'campaign_name'	=> 'Test Campaign 1', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_1->ID,
+					'campaign_name' => 'Test Campaign 1',
+					'amount'        => 10
 				),
-				array( 
-					'campaign_id' 	=> $this->campaign_2->ID,
-					'campaign_name'	=> 'Test Campaign 2', 
-					'amount'		=> 10
+				array(
+					'campaign_id'   => $this->campaign_2->ID,
+					'campaign_name' => 'Test Campaign 2',
+					'amount'        => 10
 				)
-			), 
-			'status' => 'charitable-pending'
+			),
+			'status'    => 'charitable-pending'
 		) );
 
 		$donation = charitable_get_donation( $donation_id );
@@ -377,7 +377,7 @@ class Test_Charitable_Donation extends Charitable_UnitTestCase {
 	}
 
 	public function test_get_valid_donation_statuses() {
-		$this->assertCount( 5, charitable_get_valid_donation_statuses() );	
+		$this->assertCount( 5, charitable_get_valid_donation_statuses() );
 	}
 
 }
