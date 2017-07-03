f8efc8e6e517964380594bd129b49a70d866bcdc
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:45:22 +0100

    Code consistency changes for includes/admin/views/settings/number.php

--- includes/admin/views/settings/number.php
+++ includes/admin/views/settings/number.php
@@ -16,15 +16,15 @@ if ( false === $value ) {
 $min = isset( $view_args['min'] ) ? 'min="' . $view_args['min'] . '"' : '';
 $max = isset( $view_args['max'] ) ? 'max="' . $view_args['max'] . '"' : '';
 ?>
-<input type="number" 
-	id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args['key'] ) ) ?>" 
-	name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ) ?>"
-	value="<?php echo $value ?>" <?php echo $min ?> <?php echo $max ?> 
-	class="<?php echo esc_attr( $view_args['classes'] ) ?>"
-	<?php echo charitable_get_arbitrary_attributes( $view_args ) ?>
+<input type="number"
+	id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args['key'] ) ); ?>"
+	name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ); ?>"
+	value="<?php echo $value; ?>" <?php echo $min; ?> <?php echo $max; ?>
+	class="<?php echo esc_attr( $view_args['classes'] ); ?>"
+	<?php echo charitable_get_arbitrary_attributes( $view_args ); ?>
 	/>
 <?php if ( isset( $view_args['help'] ) ) : ?>
 
-	<div class="charitable-help"><?php echo $view_args['help']  ?></div>
-	
+	<div class="charitable-help"><?php echo $view_args['help'];  ?></div>
+
 <?php endif;
