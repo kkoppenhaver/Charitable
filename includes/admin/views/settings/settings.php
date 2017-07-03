fa2e5c3d1b9a5e74255e24b674c996958a30bf8b
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:45:30 +0100

    Code consistency changes for includes/admin/views/settings/settings.php

--- includes/admin/views/settings/settings.php
+++ includes/admin/views/settings/settings.php
@@ -16,10 +16,10 @@ ob_start();
 	<h1 class="screen-reader-text"><?php echo get_admin_page_title(); ?></ha>
 	<h2 class="nav-tab-wrapper">
 		<?php foreach ( charitable_get_admin_settings()->get_sections() as $tab => $name ) : ?>
-			<a href="<?php echo esc_url( add_query_arg( array( 'tab' => $tab ), admin_url( 'admin.php?page=charitable-settings' ) ) ) ?>" class="nav-tab <?php echo $active_tab == $tab ? 'nav-tab-active' : '' ?>"><?php echo $name ?></a>
-		<?php endforeach ?>
+			<a href="<?php echo esc_url( add_query_arg( array( 'tab' => $tab ), admin_url( 'admin.php?page=charitable-settings' ) ) ); ?>" class="nav-tab <?php echo $active_tab == $tab ? 'nav-tab-active' : ''; ?>"><?php echo $name; ?></a>
+		<?php endforeach; ?>
 	</h2>
-	<?php do_action( 'charitable_before_admin_settings', $group ) ?>
+	<?php do_action( 'charitable_before_admin_settings', $group ); ?>
 	<form method="post" action="options.php">
 		<table class="form-table">
 		<?php
@@ -31,7 +31,7 @@ ob_start();
 			submit_button();
 		?>
 	</form>
-	<?php do_action( 'charitable_after_admin_settings', $group ) ?>
+	<?php do_action( 'charitable_after_admin_settings', $group ); ?>
 </div>
 <?php
 echo ob_get_clean();
