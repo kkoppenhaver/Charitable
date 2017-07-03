5be877fe4d461801ee2a6b219e62424b26a9e87b
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:44:27 +0100

    Code consistency changes for includes/admin/views/metaboxes/campaign-donation-options/suggested-amounts.php

--- includes/admin/views/metaboxes/campaign-donation-options/suggested-amounts.php
+++ includes/admin/views/metaboxes/campaign-donation-options/suggested-amounts.php
@@ -12,11 +12,11 @@ if ( ! isset( $view_args['fields'] ) || empty( $view_args['fields'] ) ) {
 	return;
 }
 
-$fields                 = $view_args['fields'];
-$title                  = isset( $view_args['label'] )      ? $view_args['label']   : '';
-$tooltip                = isset( $view_args['tooltip'] )    ? '<span class="tooltip"> '. $view_args['tooltip'] . '</span>'  : '';
-$description            = isset( $view_args['description'] )? '<span class="charitable-helper">' . $view_args['description'] . '</span>'    : '';
-$suggested_donations    = get_post_meta( $post->ID, '_campaign_suggested_donations', true );
+$fields              = $view_args['fields'];
+$title               = isset( $view_args['label'] )      ? $view_args['label']   : '';
+$tooltip             = isset( $view_args['tooltip'] )    ? '<span class="tooltip"> ' . $view_args['tooltip'] . '</span>'  : '';
+$description         = isset( $view_args['description'] )? '<span class="charitable-helper">' . $view_args['description'] . '</span>'    : '';
+$suggested_donations = get_post_meta( $post->ID, '_campaign_suggested_donations', true );
 
 if ( ! $suggested_donations ) {
 	$suggested_donations = array();
@@ -32,25 +32,25 @@ array_push( $suggested_donations, $default );
 	<table id="charitable-campaign-suggested-donations" class="widefat charitable-campaign-suggested-donations">
 		<thead>
 			<tr class="table-header">
-				<th colspan="<?php echo count( $fields ) + 2 ?>"><label for="campaign_suggested_donations"><?php echo $title ?></label></th>
+				<th colspan="<?php echo count( $fields ) + 2 ?>"><label for="campaign_suggested_donations"><?php echo $title; ?></label></th>
 			</tr>
-			<tr>                
+			<tr>
 				<?php $i = 1; ?>
 				<?php foreach ( $fields as $key => $field ) : ?>
-					<th <?php echo $i == 1 ? 'colspan="2"' : ''; ?> class="<?php echo $key ?>-col"><?php echo $field['column_header'] ?></th>
+					<th <?php echo $i == 1 ? 'colspan="2"' : ''; ?> class="<?php echo $key; ?>-col"><?php echo $field['column_header']; ?></th>
 					<?php $i++; ?>
-				<?php endforeach ?>    
-				<th class="remove-col"></th>        
+				<?php endforeach; ?>
+				<th class="remove-col"></th>
 			</tr>
-		</thead>        
+		</thead>
 		<tbody>
-			<tr class="no-suggested-amounts <?php echo ( count( $suggested_donations ) > 1 )? 'hidden' : '';?>">
-				<td colspan="<?php echo count( $fields ) + 2 ?>"><?php _e( 'No suggested amounts have been created yet.', 'charitable' ) ?></td>
+			<tr class="no-suggested-amounts <?php echo ( count( $suggested_donations ) > 1 )? 'hidden' : ''; ?>">
+				<td colspan="<?php echo count( $fields ) + 2 ?>"><?php _e( 'No suggested amounts have been created yet.', 'charitable' ); ?></td>
 			</tr>
 		<?php
 		foreach ( $suggested_donations as $i => $donation ) :
 			?>
-			<tr data-index="<?php echo $i ?>" class="<?php echo ($donation === end( $suggested_donations )) ? 'to-copy hidden' : 'default'; ?>">
+			<tr data-index="<?php echo $i; ?>" class="<?php echo ($donation === end( $suggested_donations )) ? 'to-copy hidden' : 'default'; ?>">
 
 					<td class="reorder-col"><span class="icon-donations-grab handle"></span></td>
 
@@ -69,14 +69,14 @@ array_push( $suggested_donations, $default );
 					}
 
 							?>
-							<td class="<?php echo $key ?>-col"><input 
-								type="text" 
-								class="campaign_suggested_donations" 
-								name="_campaign_suggested_donations[<?php echo $i ?>][<?php echo $key ?>]" 
-								value="<?php echo esc_attr( $value ) ?>" 
-								placeholder="<?php echo esc_attr( $field['placeholder'] ) ?>" />
+							<td class="<?php echo $key; ?>-col"><input
+								type="text"
+								class="campaign_suggested_donations"
+								name="_campaign_suggested_donations[<?php echo $i; ?>][<?php echo $key; ?>]"
+								value="<?php echo esc_attr( $value ); ?>"
+								placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>" />
 							</td>
-						<?php endforeach ?> 
+						<?php endforeach; ?>
 
 						<td class="remove-col"><span class="dashicons-before dashicons-dismiss charitable-delete-row"></span></td>
 
@@ -87,9 +87,9 @@ array_push( $suggested_donations, $default );
 		?>
 		</tbody>
 		<tfoot>
-			<tr>                
-				<td colspan="<?php echo count( $fields ) + 2 ?>"><a class="button" href="#" data-charitable-add-row="suggested-amount"><?php _e( '+ Add a Suggested Amount', 'charitable' ) ?></a></td>                
+			<tr>
+				<td colspan="<?php echo count( $fields ) + 2 ?>"><a class="button" href="#" data-charitable-add-row="suggested-amount"><?php _e( '+ Add a Suggested Amount', 'charitable' ); ?></a></td>
 			</tr>
 		</tfoot>
-	</table>    
+	</table>
 </div>
