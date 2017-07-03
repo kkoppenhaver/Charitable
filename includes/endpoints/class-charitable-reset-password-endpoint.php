b0b52629eb04cbeb5675c5be448243a2b82c4e4d
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:47:56 +0100

    Code consistency changes for includes/endpoints/class-charitable-reset-password-endpoint.php

--- includes/endpoints/class-charitable-reset-password-endpoint.php
+++ includes/endpoints/class-charitable-reset-password-endpoint.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'Charitable_Reset_Password_Endpoint' ) ) :
 		/**
 		 * Return the endpoint ID.
 		 *
-		 * @return 	string
+		 * @return string
 		 * @access 	public
 		 * @static
 		 * @since 	1.5.0
@@ -45,23 +45,20 @@ if ( ! class_exists( 'Charitable_Reset_Password_Endpoint' ) ) :
 		 * @since 	1.5.0
 		 */
 		public function setup_rewrite_rules() {
-
 			add_rewrite_endpoint( 'reset_password', EP_PERMALINK );
 			add_rewrite_rule( '(.?.+?)(?:/([0-9]+))?/reset-password/?$', 'index.php?pagename=$matches[1]&page=$matches[2]&reset_password=1', 'top' );
-
 		}
 
 		/**
 		 * Return the endpoint URL.
 		 *
 		 * @global 	WP_Rewrite $wp_rewrite
-		 * @param 	array      $args
-		 * @return  string
+		 * @param  array  $args
+		 * @return string
 		 * @access  public
 		 * @since   1.5.0
 		 */
 		public function get_page_url( $args = array() ) {
-
 			global $wp_rewrite;
 
 			$login_page = charitable_get_permalink( 'login_page' );
@@ -85,20 +82,18 @@ if ( ! class_exists( 'Charitable_Reset_Password_Endpoint' ) ) :
 			}
 
 			return esc_url_raw( add_query_arg( array( 'reset_password' => 1 ), $login_page ) );
-
 		}
 
 		/**
 		 * Return whether we are currently viewing the endpoint.
 		 *
 		 * @global  WP_Query $wp_query
-		 * @param 	array    $args
-		 * @return  boolean
+		 * @param  array   $args
+		 * @return boolean
 		 * @access  public
 		 * @since   1.5.0
 		 */
 		public function is_page( $args = array() ) {
-
 			global $wp_query;
 
 			$login_page = charitable_get_option( 'login_page', 'wp' );
@@ -109,19 +104,17 @@ if ( ! class_exists( 'Charitable_Reset_Password_Endpoint' ) ) :
 
 			return $wp_query->is_main_query()
 				&& array_key_exists( 'reset_password', $wp_query->query_vars );
-
 		}
 
 		/**
 		 * Return the template to display for this endpoint.
 		 *
-		 * @param 	string $template The default template.
-		 * @return  string
+		 * @param  string $template The default template.
+		 * @return string
 		 * @access  public
 		 * @since   1.5.0
 		 */
 		public function get_template( $template ) {
-
 			if ( 'wp' == charitable_get_option( 'login_page', 'wp' ) ) {
 				return $template;
 			}
@@ -132,19 +125,17 @@ if ( ! class_exists( 'Charitable_Reset_Password_Endpoint' ) ) :
 			) );
 
 			return array( 'reset-password-page.php', 'page.php', 'index.php' );
-
 		}
 
 		/**
 		 * Get the content to display for the endpoint.
 		 *
-		 * @param 	string $content
-		 * @return  string
+		 * @param  string $content
+		 * @return string
 		 * @access  public
 		 * @since   1.5.0
 		 */
 		public function get_content( $content ) {
-
 			ob_start();
 
 			charitable_template( 'account/reset-password.php', array(
@@ -152,8 +143,8 @@ if ( ! class_exists( 'Charitable_Reset_Password_Endpoint' ) ) :
 			) );
 
 			return ob_get_clean();
-
 		}
+
 	}
 
 endif;
