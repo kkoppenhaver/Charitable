3c23aba9769178130aac84d59705990fb4c79766
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:49:20 +0100

    Code consistency changes for includes/users/class-charitable-roles.php

--- includes/users/class-charitable-roles.php
+++ includes/users/class-charitable-roles.php
@@ -24,45 +24,45 @@ if ( ! class_exists( 'Charitable_Roles' ) ) :
 		/**
 		 * Sets up roles for Charitable. This is called by the install script.
 		 *
-		 * @return 	void
+		 * @return void
 		 * @since 	1.0.0
 		 */
 		public function add_roles() {
 			add_role( 'campaign_manager', __( 'Campaign Manager', 'charitable' ), array(
-				'read' 						=> true,
-				'delete_posts' 				=> true,
-				'edit_posts' 				=> true,
-				'delete_published_posts' 	=> true,
-				'publish_posts' 			=> true,
-				'upload_files' 				=> true,
-				'edit_published_posts' 		=> true,
-				'read_private_pages' 		=> true,
-				'edit_private_pages' 		=> true,
-				'delete_private_pages' 		=> true,
-				'read_private_posts' 		=> true,
-				'edit_private_posts' 		=> true,
-				'delete_private_posts' 		=> true,
-				'delete_others_posts' 		=> true,
-				'delete_published_pages' 	=> true,
-				'delete_others_pages' 		=> true,
-				'delete_pages' 				=> true,
-				'publish_pages' 			=> true,
-				'edit_published_pages' 		=> true,
-				'edit_others_pages' 		=> true,
-				'edit_pages' 				=> true,
-				'edit_others_posts' 		=> true,
-				'manage_links' 				=> true,
-				'manage_categories' 		=> true,
-				'moderate_comments' 		=> true,
-				'import' 					=> true,
-				'export' 					=> true,
-				'unfiltered_html'		 	=> true,
+				'read'                   => true,
+				'delete_posts'           => true,
+				'edit_posts'             => true,
+				'delete_published_posts' => true,
+				'publish_posts'          => true,
+				'upload_files'           => true,
+				'edit_published_posts'   => true,
+				'read_private_pages'     => true,
+				'edit_private_pages'     => true,
+				'delete_private_pages'   => true,
+				'read_private_posts'     => true,
+				'edit_private_posts'     => true,
+				'delete_private_posts'   => true,
+				'delete_others_posts'    => true,
+				'delete_published_pages' => true,
+				'delete_others_pages'    => true,
+				'delete_pages'           => true,
+				'publish_pages'          => true,
+				'edit_published_pages'   => true,
+				'edit_others_pages'      => true,
+				'edit_pages'             => true,
+				'edit_others_posts'      => true,
+				'manage_links'           => true,
+				'manage_categories'      => true,
+				'moderate_comments'      => true,
+				'import'                 => true,
+				'export'                 => true,
+				'unfiltered_html'        => true,
 			) );
 
 			add_role( 'donor', __( 'Donor', 'charitable' ), array(
-				'read' 						=> true,
-				'edit_posts' 				=> false,
-				'delete_posts' 				=> false,
+				'read'         => true,
+				'edit_posts'   => false,
+				'delete_posts' => false,
 			) );
 		}
 
@@ -70,7 +70,7 @@ if ( ! class_exists( 'Charitable_Roles' ) ) :
 		 * Sets up capabilities for Charitable. This is called by the install script.
 		 *
 		 * @global 	WP_Roles
-		 * @return 	void
+		 * @return void
 		 * @since 	1.0.0
 		 */
 		public function add_caps() {
@@ -103,7 +103,7 @@ if ( ! class_exists( 'Charitable_Roles' ) ) :
 		 * Removes roles. This is called upon deactivation.
 		 *
 		 * @global 	WP_Roles
-		 * @return 	void
+		 * @return void
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
@@ -139,7 +139,7 @@ if ( ! class_exists( 'Charitable_Roles' ) ) :
 		/**
 		 * Returns the caps for the post types that Charitable adds.
 		 *
-		 * @return 	array
+		 * @return array
 		 * @access 	private
 		 * @since 	1.0.0
 		 */
@@ -182,6 +182,7 @@ if ( ! class_exists( 'Charitable_Roles' ) ) :
 				'assign_campaign_terms',
 			);
 		}
+
 	}
 
 endif;
