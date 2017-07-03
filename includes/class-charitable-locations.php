05ac75f18494f4a58fa7962e04b325781f527093
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:46:03 +0100

    Code consistency changes for includes/class-charitable-locations.php

--- includes/class-charitable-locations.php
+++ includes/class-charitable-locations.php
@@ -59,7 +59,7 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Returns and/or create the single instance of this class.
 		 *
-		 * @return  Charitable_Locations
+		 * @return Charitable_Locations
 		 * @access  public
 		 * @since   1.2.0
 		 */
@@ -82,7 +82,7 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Return an array with all the countries supported by Charitable.
 		 *
-		 * @return 	string[]
+		 * @return string[]
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
@@ -97,7 +97,7 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Return the country codes of countries with states.
 		 *
-		 * @return 	string[]
+		 * @return string[]
 		 * @access  public
 		 * @since 	1.0.0
 		 */
@@ -133,12 +133,11 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Return an array with all the states supported by Charitable.
 		 *
-		 * @return 	string[]
+		 * @return string[]
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
 		public function get_all_states() {
-
 			foreach ( $this->get_countries_with_states() as $country_code ) {
 				$this->get_states_for_country( $country_code );
 			}
@@ -149,13 +148,12 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Return the states in this country.
 		 *
-		 * @param 	string $country_code The three-character country code.
-		 * @return 	string[]
+		 * @param  string   $country_code The three-character country code.
+		 * @return string[]
 		 * @access  public
 		 * @since 	1.0.0
 		 */
 		public function get_states_for_country( $country_code ) {
-
 			if ( ! in_array( $country_code, $this->get_countries_with_states() ) ) {
 				return array();
 			}
@@ -170,7 +168,7 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Get the base country for the website.
 		 *
-		 * @return 	string
+		 * @return string
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
@@ -183,51 +181,50 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Get country address formats.
 		 *
-		 * @return 	string[]
+		 * @return string[]
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
 		public function get_address_formats() {
-
 			if ( ! isset( $this->address_formats ) ) {
 
 				// Common formats.
 				$postcode_before_city = "{company}\n{name}\n{address_1}\n{address_2}\n{postcode} {city}\n{country}";
 
 				// Define address formats.
-				$this->address_formats = apply_filters('charitable_localisation_address_formats', array(
+				$this->address_formats = apply_filters( 'charitable_localisation_address_formats', array(
 					'default' => "{name}\n{company}\n{address_1}\n{address_2}\n{city}\n{state}\n{postcode}\n{country}",
-					'AU' => "{name}\n{company}\n{address_1}\n{address_2}\n{city} {state} {postcode}\n{country}",
-					'AT' => $postcode_before_city,
-					'BE' => $postcode_before_city,
-					'CA' => "{company}\n{name}\n{address_1}\n{address_2}\n{city} {state} {postcode}\n{country}",
-					'CH' => $postcode_before_city,
-					'CN' => "{country} {postcode}\n{state}, {city}, {address_2}, {address_1}\n{company}\n{name}",
-					'CZ' => $postcode_before_city,
-					'DE' => $postcode_before_city,
-					'EE' => $postcode_before_city,
-					'FI' => $postcode_before_city,
-					'DK' => $postcode_before_city,
-					'FR' => "{company}\n{name}\n{address_1}\n{address_2}\n{postcode} {city_upper}\n{country}",
-					'HK' => "{company}\n{first_name} {last_name_upper}\n{address_1}\n{address_2}\n{city_upper}\n{state_upper}\n{country}",
-					'HU' => "{name}\n{company}\n{city}\n{address_1}\n{address_2}\n{postcode}\n{country}",
-					'IS' => $postcode_before_city,
-					'IT' => "{company}\n{name}\n{address_1}\n{address_2}\n{postcode}\n{city}\n{state_upper}\n{country}",
-					'JP' => "{postcode}\n{state}{city}{address_1}\n{address_2}\n{company}\n{last_name} {first_name}\n {country}",
-					'TW' => "{postcode}\n{city}{address_2}\n{address_1}\n{company}\n{last_name} {first_name}\n {country}",
-					'LI' => $postcode_before_city,
-					'NL' => $postcode_before_city,
-					'NZ' => "{name}\n{company}\n{address_1}\n{address_2}\n{city} {postcode}\n{country}",
-					'NO' => $postcode_before_city,
-					'PL' => $postcode_before_city,
-					'SK' => $postcode_before_city,
-					'SI' => $postcode_before_city,
-					'ES' => "{name}\n{company}\n{address_1}\n{address_2}\n{postcode} {city}\n{state}\n{country}",
-					'SE' => $postcode_before_city,
-					'TR' => "{name}\n{company}\n{address_1}\n{address_2}\n{postcode} {city} {state}\n{country}",
-					'US' => "{name}\n{company}\n{address_1}\n{address_2}\n{city}, {state_code} {postcode}\n{country}",
-					'VN' => "{name}\n{company}\n{address_1}\n{city}\n{country}",
-				));
+					'AU'      => "{name}\n{company}\n{address_1}\n{address_2}\n{city} {state} {postcode}\n{country}",
+					'AT'      => $postcode_before_city,
+					'BE'      => $postcode_before_city,
+					'CA'      => "{company}\n{name}\n{address_1}\n{address_2}\n{city} {state} {postcode}\n{country}",
+					'CH'      => $postcode_before_city,
+					'CN'      => "{country} {postcode}\n{state}, {city}, {address_2}, {address_1}\n{company}\n{name}",
+					'CZ'      => $postcode_before_city,
+					'DE'      => $postcode_before_city,
+					'EE'      => $postcode_before_city,
+					'FI'      => $postcode_before_city,
+					'DK'      => $postcode_before_city,
+					'FR'      => "{company}\n{name}\n{address_1}\n{address_2}\n{postcode} {city_upper}\n{country}",
+					'HK'      => "{company}\n{first_name} {last_name_upper}\n{address_1}\n{address_2}\n{city_upper}\n{state_upper}\n{country}",
+					'HU'      => "{name}\n{company}\n{city}\n{address_1}\n{address_2}\n{postcode}\n{country}",
+					'IS'      => $postcode_before_city,
+					'IT'      => "{company}\n{name}\n{address_1}\n{address_2}\n{postcode}\n{city}\n{state_upper}\n{country}",
+					'JP'      => "{postcode}\n{state}{city}{address_1}\n{address_2}\n{company}\n{last_name} {first_name}\n {country}",
+					'TW'      => "{postcode}\n{city}{address_2}\n{address_1}\n{company}\n{last_name} {first_name}\n {country}",
+					'LI'      => $postcode_before_city,
+					'NL'      => $postcode_before_city,
+					'NZ'      => "{name}\n{company}\n{address_1}\n{address_2}\n{city} {postcode}\n{country}",
+					'NO'      => $postcode_before_city,
+					'PL'      => $postcode_before_city,
+					'SK'      => $postcode_before_city,
+					'SI'      => $postcode_before_city,
+					'ES'      => "{name}\n{company}\n{address_1}\n{address_2}\n{postcode} {city}\n{state}\n{country}",
+					'SE'      => $postcode_before_city,
+					'TR'      => "{name}\n{company}\n{address_1}\n{address_2}\n{postcode} {city} {state}\n{country}",
+					'US'      => "{name}\n{company}\n{address_1}\n{address_2}\n{city}, {state_code} {postcode}\n{country}",
+					'VN'      => "{name}\n{company}\n{address_1}\n{city}\n{country}",
+				) );
 
 			}//end if
 
@@ -237,13 +234,12 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 		/**
 		 * Return the address format to use for the given address.
 		 *
-		 * @param 	array $address_fields Address fields.
-		 * @return 	string
+		 * @param  array  $address_fields Address fields.
+		 * @return string
 		 * @access  private
 		 * @since 	1.0.0
 		 */
 		private function get_address_format( $address_fields ) {
-
 			$formats = $this->get_address_formats();
 			$country = $address_fields['country'];
 
@@ -262,19 +258,17 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 			}
 
 			return $format;
-
 		}
 
 		/**
 		 * Process an array of address fields, trimming whitespace and adding full country and full state names.
 		 *
-		 * @param 	array $address_fields Address fields.
-		 * @return 	string[]
+		 * @param  array    $address_fields Address fields.
+		 * @return string[]
 		 * @access  private
 		 * @since 	1.0.0
 		 */
 		private function sanitize_address_fields( $address_fields ) {
-
 			if ( ! is_array( $address_fields ) ) {
 				return $address_fields;
 			}
@@ -287,14 +281,14 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 			$defaults = array(
 				'first_name'   => '',
 				'last_name'    => '',
-				'company'	   => '',
-				'address'	   => '',
+				'company'      => '',
+				'address'      => '',
 				'address_2'    => '',
-				'city' 		   => '',
-				'state'		   => '',
+				'city'         => '',
+				'state'        => '',
 				'full_state'   => '',
-				'postcode'	   => '',
-				'country'	   => '',
+				'postcode'     => '',
+				'country'      => '',
 				'full_country' => '',
 			);
 
@@ -329,19 +323,17 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 			}
 
 			return $address_fields;
-
 		}
 
 		/**
 		 * Get formatted address based on country of address.
 		 *
-		 * @param 	array $address_fields Address fields.
-		 * @return 	string
+		 * @param  array  $address_fields Address fields.
+		 * @return string
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
 		public function get_formatted_address( $address_fields = array() ) {
-
 			$address_fields    = $this->sanitize_address_fields( $address_fields );
 			$formatted_address = '';
 
@@ -375,7 +367,7 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 				'{country_upper}'    => strtoupper( $address_fields['full_country'] ),
 			), $address_fields ) );
 
-			$format = $this->get_address_format( $address_fields );
+			$format            = $this->get_address_format( $address_fields );
 			$formatted_address = str_replace( array_keys( $replace ), $replace, $format );
 
 			/* Clean up white space */
@@ -391,20 +383,20 @@ if ( ! class_exists( 'Charitable_Locations' ) ) :
 			$formatted_address = implode( '<br/>', $formatted_address );
 
 			return $formatted_address;
-
 		}
 
 		/**
 		 * Trim white space and commas off a line.
 		 *
-		 * @param  	string $line The line to be trimmed.
-		 * @return 	string
+		 * @param  string $line The line to be trimmed.
+		 * @return string
 		 * @access 	private
 		 * @since 	1.0.0
 		 */
 		private function trim_formatted_address_line( $line ) {
 			return trim( $line, ', ' );
 		}
+
 	}
 
 endif;
