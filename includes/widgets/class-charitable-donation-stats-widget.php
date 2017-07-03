2bece615be3d7e039993a36ef42046ca8b5d5665
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:49:31 +0100

    Code consistency changes for includes/widgets/class-charitable-donation-stats-widget.php

--- includes/widgets/class-charitable-donation-stats-widget.php
+++ includes/widgets/class-charitable-donation-stats-widget.php
@@ -21,78 +21,79 @@ if ( ! class_exists( 'Charitable_Donation_Stats_Widget' ) ) :
  */
 class Charitable_Donation_Stats_Widget extends WP_Widget {
 
-    /**
-     * Instantiate the widget and set up basic configuration.
-     *
-     * @access  public
-     * @since   1.0.0
-     */
-    public function __construct() {
-        parent::__construct(
-            'charitable_donation_stats_widget', 
-            __( 'Donation Stats', 'charitable' ),             
-            array( 
-                'description' => __( 'Show off your donation statistics.', 'charitable' ),
-                'customize_selective_refresh' => true 
-            )
-        );
-    }
+	/**
+	 * Instantiate the widget and set up basic configuration.
+	 *
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function __construct() {
+		parent::__construct(
+			'charitable_donation_stats_widget',
+			__( 'Donation Stats', 'charitable' ),
+			array(
+				'description'                 => __( 'Show off your donation statistics.', 'charitable' ),
+				'customize_selective_refresh' => true
+			)
+		);
+	}
 
-    /**
-     * Display the widget contents on the front-end.
+	/**
+	 * Display the widget contents on the front-end.
 
-     *
-     * @param   array $args
-     * @param   array $instance
-     * @access  public 
-     * @since   1.0.0
-     */
-    public function widget( $args, $instance ) {
-        $view_args = array_merge( $args, $instance );
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function widget( $args, $instance ) {
+		$view_args = array_merge( $args, $instance );
 
-        if ( ! isset( $view_args[ 'title' ] ) ) {
-            $view_args[ 'title' ] = __( 'Donation Statistics', 'charitable' );
-        }
+		if ( ! isset( $view_args['title'] ) ) {
+			$view_args['title'] = __( 'Donation Statistics', 'charitable' );
+		}
 
-        charitable_template( 'widgets/donation-stats.php', $view_args );
-    }    
+		charitable_template( 'widgets/donation-stats.php', $view_args );
+	}
 
-    /**
-     * Display the widget form in the admin.
-     *
-     * @param   array $instance         The current settings for the widget options.
+	/**
+	 * Display the widget form in the admin.
+	 *
+	 * @param  array $instance The current settings for the widget options.
 
-     * @return  void
-     * @access  public
-     * @since   1.0.0
-     */
-    public function form( $instance ) {
-        $title = isset( $instance[ 'title' ] ) ? esc_attr( $instance[ 'title' ] ) : '';
-        ?>
+	 * @return void
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function form( $instance ) {
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		?>
         <p>
             <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'charitable' ); ?></label>
-            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' )  ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
         </p>
         <?php
-    }
+	}
 
-    /**
-     * Update the widget settings in the admin.
+	/**
+	 * Update the widget settings in the admin.
 
-     *
-     * @param   array $new_instance         The updated settings.
+	 *
+	 * @param  array $new_instance The updated settings.
 
-     * @param   array $new_instance         The old settings.
+	 * @param  array $new_instance The old settings.
 
-     * @return  void
-     * @access  public
-     * @since   1.0.0
-     */
-    public function update( $new_instance, $old_instance ) {
-        $instance = array();
-        $instance[ 'title' ] = isset( $new_instance[ 'title' ] ) ? strip_tags( $new_instance[ 'title' ] ) : $old_instance[ 'title' ];        
-        return $instance;
-    }
+	 * @return void
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance          = array();
+		$instance['title'] = isset( $new_instance['title'] ) ? strip_tags( $new_instance['title'] ) : $old_instance['title'];
+		return $instance;
+	}
+
 }
 
 endif;
