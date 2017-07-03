410dd1cae17c2e6922c178cdf8873afc47357f3d
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:51:32 +0100

    Code consistency changes for templates/form-fields/number.php

--- templates/form-fields/number.php
+++ templates/form-fields/number.php
@@ -12,21 +12,21 @@ if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
 	return;
 }
 
-$form 		 = $view_args['form'];
-$field 		 = $view_args['field'];
-$classes 	 = $view_args['classes'];
+$form        = $view_args['form'];
+$field       = $view_args['field'];
+$classes     = $view_args['classes'];
 $is_required = isset( $field['required'] ) ? $field['required'] : false;
-$value		 = isset( $field['value'] ) ? $field['value'] : '';
+$value       = isset( $field['value'] ) ? $field['value'] : '';
 
 ?>
-<div id="charitable_field_<?php echo $field['key'] ?>" class="<?php echo $classes ?>">	
+<div id="charitable_field_<?php echo $field['key']; ?>" class="<?php echo $classes; ?>">
 	<?php if ( isset( $field['label'] ) ) : ?>
-		<label for="charitable_field_<?php echo $field['key'] ?>_element">
-			<?php echo $field['label'] ?>			
+		<label for="charitable_field_<?php echo $field['key']; ?>_element">
+			<?php echo $field['label']; ?>
 			<?php if ( $is_required ) : ?>
 				<abbr class="required" title="required">*</abbr>
-			<?php endif ?>
+			<?php endif; ?>
 		</label>
-	<?php endif ?>
-	<input type="number" name="<?php echo esc_attr( $field['key'] ) ?>" id="charitable_field_<?php echo $field['key'] ?>_element" value="<?php echo esc_attr( $value ) ?>" <?php echo charitable_get_arbitrary_attributes( $field ) ?>/>
+	<?php endif; ?>
+	<input type="number" name="<?php echo esc_attr( $field['key'] ); ?>" id="charitable_field_<?php echo $field['key']; ?>_element" value="<?php echo esc_attr( $value ); ?>" <?php echo charitable_get_arbitrary_attributes( $field ); ?>/>
 </div>
