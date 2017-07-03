81b1a66e33c184834ada4151f53a6b4e2544a2c1
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:46:01 +0100

    Code consistency changes for includes/class-charitable-install.php

--- includes/class-charitable-install.php
+++ includes/class-charitable-install.php
@@ -40,7 +40,7 @@ if ( ! class_exists( 'Charitable_Install' ) ) :
 		/**
 		 * Finish the plugin installation.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @static
 		 * @since   1.3.4
@@ -54,7 +54,7 @@ if ( ! class_exists( 'Charitable_Install' ) ) :
 		/**
 		 * Create wp roles and assign capabilities
 		 *
-		 * @return 	void
+		 * @return void
 		 * @access 	protected
 		 * @since 	1.0.0
 		 */
@@ -68,7 +68,7 @@ if ( ! class_exists( 'Charitable_Install' ) ) :
 		/**
 		 * Create database tables.
 		 *
-		 * @return 	void
+		 * @return void
 		 * @access 	protected
 		 * @since 	1.0.0
 		 */
@@ -87,7 +87,7 @@ if ( ! class_exists( 'Charitable_Install' ) ) :
 		/**
 		 * Set up the upgrade log.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  protected
 		 * @since   1.3.0
 		 */
@@ -95,6 +95,7 @@ if ( ! class_exists( 'Charitable_Install' ) ) :
 			require_once( 'admin/upgrades/class-charitable-upgrade.php' );
 			Charitable_Upgrade::get_instance()->populate_upgrade_log_on_install();
 		}
+
 	}
 
 endif;
