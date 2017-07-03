5433d2c6afc0e936ae828bd546900d6ff84dea91
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:45:29 +0100

    Code consistency changes for includes/admin/views/settings/select.php

--- includes/admin/views/settings/select.php
+++ includes/admin/views/settings/select.php
@@ -13,42 +13,42 @@ if ( empty( $value ) ) {
 	$value = isset( $view_args['default'] ) ? $view_args['default'] : '';
 }
 ?>
-<select id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args['key'] ) ) ?>" 
-	name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ) ?>"
-	class="<?php echo esc_attr( $view_args['classes'] ) ?>"
-	<?php echo charitable_get_arbitrary_attributes( $view_args ) ?>
-	>	
+<select id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args['key'] ) ); ?>"
+	name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ); ?>"
+	class="<?php echo esc_attr( $view_args['classes'] ); ?>"
+	<?php echo charitable_get_arbitrary_attributes( $view_args ); ?>
+	>
 	<?php
 
 	foreach ( $view_args['options'] as $key => $option ) :
 
 		if ( is_array( $option ) ) :
 
-			$label = isset( $option['label'] ) ? $option['label'] : '' ?>
+			$label = isset( $option['label'] ) ? $option['label'] : ''; ?>
 
-			<optgroup label="<?php echo $label ?>">
+			<optgroup label="<?php echo $label; ?>">
 
 			<?php foreach ( $option['options'] as $k => $opt ) : ?>
 
-				<option value="<?php echo $k ?>" <?php selected( $k, $value ) ?>><?php echo $opt ?></option>
+				<option value="<?php echo $k; ?>" <?php selected( $k, $value ); ?>><?php echo $opt; ?></option>
 
-			<?php endforeach ?>
+			<?php endforeach; ?>
 
 			</optgroup>
 
 		<?php else : ?>
-			
-			<option value="<?php echo $key ?>" <?php selected( $key, $value ) ?>><?php echo $option ?></option>
+
+			<option value="<?php echo $key; ?>" <?php selected( $key, $value ); ?>><?php echo $option; ?></option>
 
 		<?php
 
 		endif;
-	endforeach
+	endforeach;
 
 	?>
 </select>
 <?php if ( isset( $view_args['help'] ) ) : ?>
 
-	<div class="charitable-help"><?php echo $view_args['help']  ?></div>
-	
+	<div class="charitable-help"><?php echo $view_args['help'];  ?></div>
+
 <?php endif;
