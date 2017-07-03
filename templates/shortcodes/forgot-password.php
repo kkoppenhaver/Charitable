2f7c050b50bbf76bd914aa97402e9d43ddfdd42b
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:51:49 +0100

    Code consistency changes for templates/shortcodes/forgot-password.php

--- templates/shortcodes/forgot-password.php
+++ templates/shortcodes/forgot-password.php
@@ -17,14 +17,14 @@ $form = $view_args['form'];
 <div class="charitable-forgot-password-form">
 	<?php
 	/**
-	* @hook charitable_forgot_password_before
-	*/
+	 * @hook charitable_forgot_password_before
+	 */
 	do_action( 'charitable_forgot_password_before' );
 
 	?>
-	<form id="lostpasswordform" class="charitable-form" action="<?php echo wp_lostpassword_url() ?>" method="post">
+	<form id="lostpasswordform" class="charitable-form" action="<?php echo wp_lostpassword_url(); ?>" method="post">
 
-		<?php do_action( 'charitable_form_before_fields', $form ) ?>
+		<?php do_action( 'charitable_form_before_fields', $form ); ?>
 
 		<div class="charitable-form-fields cf">
 			<?php
@@ -39,16 +39,16 @@ $form = $view_args['form'];
 		</div>
 
 		<?php do_action( 'charitable_form_after_fields', $form ); ?>
-	
+
 		<div class="charitable-form-field charitable-submit-field">
-			<input type="submit" name="submit" class="lostpassword-button" value="<?php esc_attr_e( 'Reset Password', 'charitable' ) ?>" />
+			<input type="submit" name="submit" class="lostpassword-button" value="<?php esc_attr_e( 'Reset Password', 'charitable' ); ?>" />
 		</div>
 
 	</form>
 	<?php
 	/**
-	* @hook charitable_forgot_password_after
-	*/
+	 * @hook charitable_forgot_password_after
+	 */
 	do_action( 'charitable_forgot_password_after' );
 	?>
 </div>
