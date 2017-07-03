a76fc2617d5619b6333dc2681bfc1b60c1dcd97c
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:48:44 +0100

    Code consistency changes for includes/shortcodes/charitable-shortcodes-hooks.php

--- includes/shortcodes/charitable-shortcodes-hooks.php
+++ includes/shortcodes/charitable-shortcodes-hooks.php
@@ -24,15 +24,15 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @see     Charitable_Registration_Shortcode::display()
  * @see     Charitable_Profile_Shortcode::display()
  */
-add_shortcode( 'campaigns',               array( 'Charitable_Campaigns_Shortcode', 'display' ) );
-add_shortcode( 'donation_receipt',        array( 'Charitable_Donation_Receipt_Shortcode', 'display' ) );
+add_shortcode( 'campaigns', array( 'Charitable_Campaigns_Shortcode', 'display' ) );
+add_shortcode( 'donation_receipt', array( 'Charitable_Donation_Receipt_Shortcode', 'display' ) );
 add_shortcode( 'charitable_my_donations', array( 'Charitable_My_Donations_Shortcode', 'display' ) );
-add_shortcode( 'charitable_login',        array( 'Charitable_Login_Shortcode', 'display' ) );
+add_shortcode( 'charitable_login', array( 'Charitable_Login_Shortcode', 'display' ) );
 add_shortcode( 'charitable_registration', array( 'Charitable_Registration_Shortcode', 'display' ) );
-add_shortcode( 'charitable_profile',      array( 'Charitable_Profile_Shortcode', 'display' ) );
+add_shortcode( 'charitable_profile', array( 'Charitable_Profile_Shortcode', 'display' ) );
 
 /**
- * Fingerprint the login form with our charitable=true hidden field 
+ * Fingerprint the login form with our charitable=true hidden field
  *
  * @see     Charitable_Login_Shortcode::add_hidden_field_to_login_form()
  */
