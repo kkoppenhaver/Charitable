74b7bd5f0e736f7a2c5041c1c4188eea1b48c5c4
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:52:12 +0100

    Code consistency changes for tests/helpers/charitable-donation-helper.php

--- tests/helpers/charitable-donation-helper.php
+++ tests/helpers/charitable-donation-helper.php
@@ -7,9 +7,9 @@
 class Charitable_Donation_Helper extends WP_UnitTestCase {
 
 	/**
-	 * Delete a donation 
+	 * Delete a donation
 	 *
-	 * @return 	void
+	 * @return void
 	 * @access  public
 	 * @since 	1.0.0
 	 */
@@ -20,19 +20,19 @@ class Charitable_Donation_Helper extends WP_UnitTestCase {
 	/**
 	 * Create a donation.
 	 *
-	 * @param 	array 		$args 				Optional arguments.
-	 * @return 	int 		$donation_id
+	 * @param  array $args        Optional arguments.
+	 * @return int   $donation_id
 	 * @access  public
 	 * @static
-	 * @since 	1.0.0	 
+	 * @since 	1.0.0
 	 */
 	public static function create_donation( $args = array() ) {
 		$defaults = array(
-			'user_id'		=> 1, 
-			'campaigns'		=> array(), 
-			'status'		=> 'charitable-completed', 
-			'gateway'		=> 'manual', 
-			'note'			=> ''
+			'user_id'   => 1,
+			'campaigns' => array(),
+			'status'    => 'charitable-completed',
+			'gateway'   => 'manual',
+			'note'      => ''
 		);
 
 		$args = array_merge( $defaults, $args );
@@ -51,26 +51,27 @@ class Charitable_Donation_Helper extends WP_UnitTestCase {
 	/**
 	 * Create a donation for a user.
 	 *
-	 * @param 	int 		$user_id
-	 * @param 	int 		$campaign_id
-	 * @param 	float 		$amount
-	 * @return 	int 		$donation_id
+	 * @param  int   $user_id
+	 * @param  int   $campaign_id
+	 * @param  float $amount
+	 * @return int   $donation_id
 	 * @access  public
 	 * @static
 	 * @since 	1.0.0
 	 */
 	public static function create_campaign_donation_for_user( $user_id, $campaign_id, $amount ) {
-	 	$args = array(
-	 		'donor_id'			=> $user_id, 
-	 		'user_id'			=> $user_id, 
-	 		'campaigns'			=> array(
-	 			array(	 		
-		 			'campaign_id' 	=> $campaign_id, 
-		 			'amount'		=> $amount
-		 		)
-	 		)
-	 	);
+		$args = array(
+			'donor_id'  => $user_id,
+			'user_id'   => $user_id,
+			'campaigns' => array(
+				array(
+					'campaign_id' => $campaign_id,
+					'amount'      => $amount
+				)
+			)
+		);
 
-	 	return self::create_donation( $args );
+		return self::create_donation( $args );
 	}
+
 }
