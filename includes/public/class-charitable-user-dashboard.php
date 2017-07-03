1327069a44df4191db981fcff7061c4c9aec460f
ericnicolaas <eric@ericnicolaas.com>
2017-07-03 08:48:42 +0100

    Code consistency changes for includes/public/class-charitable-user-dashboard.php

--- includes/public/class-charitable-user-dashboard.php
+++ includes/public/class-charitable-user-dashboard.php
@@ -33,7 +33,7 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Returns and/or create the single instance of this class.
 		 *
-		 * @return  Charitable_User_Dashboard
+		 * @return Charitable_User_Dashboard
 		 * @access  public
 		 * @since   1.2.0
 		 */
@@ -64,7 +64,7 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Register navigation menu for frontend dashboard.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -76,8 +76,8 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		 * Returns the user dashboard navigation menu.
 		 *
 		 * @uses    wp_nav_menu
-		 * @param   array       $args
-		 * @return  void
+		 * @param  array $args
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -95,7 +95,7 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Return the menu ID based on the theme location.
 		 *
-		 * @return  int         0 if no menu found. Menu ID otherwise.
+		 * @return int 0 if no menu found. Menu ID otherwise.
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -113,7 +113,7 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		 * Returns all objects in the user dashboard navigation.
 		 *
 		 * @uses    wp_get_nav_menu_items
-		 * @return  WP_Post[]
+		 * @return WP_Post[]
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -156,7 +156,7 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Flushes the menu object cache after updating a menu or menu item.
 		 *
-		 * @return  void
+		 * @return void
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -173,8 +173,8 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Checks whether the current requested page is in the user dashboard nav.
 		 *
-		 * @param   Object $object Optional. If not set, will base it on the current queried object.
-		 * @return  boolean
+		 * @param  Object  $object Optional. If not set, will base it on the current queried object.
+		 * @return boolean
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -203,8 +203,8 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Loads the user dashboard template.
 		 *
-		 * @param   string $template
-		 * @return  string
+		 * @param  string $template
+		 * @return string
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -234,8 +234,8 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 		/**
 		 * Add the user-dashboard class to the body if we're looking at it.
 		 *
-		 * @param   array       $classes
-		 * @return  array
+		 * @param  array $classes
+		 * @return array
 		 * @access  public
 		 * @since   1.0.0
 		 */
@@ -248,6 +248,7 @@ if ( ! class_exists( 'Charitable_User_Dashboard' ) ) :
 
 			return $classes;
 		}
+
 	}
 
 endif;
