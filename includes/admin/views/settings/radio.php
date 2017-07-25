126e9d95e056ba7d0d44d5cca993de0e050e0b24
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:32:02 +1000

    Code consistency changes for includes/admin/views/settings/radio.php

--- includes/admin/views/settings/radio.php
+++ includes/admin/views/settings/radio.php
@@ -14,25 +14,25 @@ if ( empty( $value ) ) :
 endif;
 
 ?>
-<ul class="charitable-radio-list <?php echo esc_attr( $view_args['classes'] ) ?>">
+<ul class="charitable-radio-list <?php echo esc_attr( $view_args['classes'] ); ?>">
 
 	<?php foreach ( $view_args['options'] as $option => $label ) : ?>
 
-		<li><input type="radio" 
-				id="<?php printf( 'charitable_settings_%s_%s', implode( '_', $view_args['key'] ), $option ) ?>" 
-				name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ) ?>"
-				value="<?php echo esc_attr( $option ) ?>"
-				<?php checked( $value, $option ) ?> 
-				<?php echo charitable_get_arbitrary_attributes( $view_args ) ?>
+		<li><input type="radio"
+				id="<?php printf( 'charitable_settings_%s_%s', implode( '_', $view_args['key'] ), $option ); ?>"
+				name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ); ?>"
+				value="<?php echo esc_attr( $option ); ?>"
+				<?php checked( $value, $option ); ?>
+				<?php echo charitable_get_arbitrary_attributes( $view_args ); ?>
 				/>
-			<?php echo $label ?>
+			<?php echo $label; ?>
 		</li>
 
-	<?php endforeach ?>
+	<?php endforeach; ?>
 
 </ul>
 <?php if ( isset( $view_args['help'] ) ) : ?>
 
-	<div class="charitable-help"><?php echo $view_args['help']  ?></div>
+	<div class="charitable-help"><?php echo $view_args['help'];  ?></div>
 
 <?php endif;
