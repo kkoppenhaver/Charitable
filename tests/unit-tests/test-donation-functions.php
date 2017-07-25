b52a39ab5488e5eb4dc38f7df530f5da73c569ff
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:39:09 +1000

    Code consistency changes for tests/unit-tests/test-donation-functions.php

--- tests/unit-tests/test-donation-functions.php
+++ tests/unit-tests/test-donation-functions.php
@@ -14,11 +14,9 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 	 * @covers charitable_create_donation
 	 */
 	public function test_create_donation() {
-
 		$donation_id = $this->create_donation();
 
 		$this->assertGreaterThan( 0, $donation_id );
-
 	}
 
 	/**
@@ -26,11 +24,9 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 	 * @depends test_create_donation
 	 */
 	public function test_create_donation_correct_gateway() {
-
 		$donation_id = $this->create_donation();
 
 		$this->assertEquals( 'manual', charitable_get_donation_gateway( $donation_id ) );
-
 	}
 
 	/**
@@ -38,11 +34,9 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 	 * @depends test_create_donation
 	 */
 	public function test_create_donation_correct_amount() {
-
 		$donation_id = $this->create_donation();
 
 		$this->assertEquals( 50, charitable_get_donation( $donation_id )->get_total_donation_amount( false ) );
-
 	}
 
 	/**
@@ -50,11 +44,9 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 	 * @depends test_create_donation
 	 */
 	public function test_create_donation_correct_user() {
-
 		$donation_id = $this->create_donation();
 
 		$this->assertEquals( 'Matthew Murdoch', charitable_get_donation( $donation_id )->get_donor() );
-
 	}
 
 	/**
@@ -62,7 +54,6 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 	 * @depends test_create_donation
 	 */
 	public function test_cancel_donation() {
-
 		/**
 		 * Temporary workaround for issue noted below.
 		 * @see https://core.trac.wordpress.org/ticket/37207
@@ -76,7 +67,6 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 		$this->go_to( $page );
 
 		$this->assertTrue( charitable_cancel_donation() );
-
 	}
 
 	/**
@@ -84,7 +74,6 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 	 * @depends test_cancel_donation
 	 */
 	public function test_do_not_cancel_donation() {
-
 		/**
 		 * Temporary workaround for issue noted below.
 		 * @see https://core.trac.wordpress.org/ticket/37207
@@ -100,38 +89,36 @@ class Test_Charitable_Donation_Functions extends Charitable_UnitTestCase {
 		$this->go_to( $donate_url );
 
 		$this->assertFalse( charitable_cancel_donation() );
-
 	}
 
 	/**
 	 * Create a donation using `charitable_create_donation()`
 	 *
-	 * @param 	string $status
-	 * @return  int The donation ID
+	 * @param  string $status
+	 * @return int            The donation ID
 	 */
 	private function create_donation( $status = 'charitable-completed' ) {
-
 		$campaign_id = Charitable_Campaign_Helper::create_campaign();
 
 		$args = array(
-			'status' => $status,
-			'gateway' => 'manual',
-			'note' => '',
+			'status'    => $status,
+			'gateway'   => 'manual',
+			'note'      => '',
 			'campaigns' => array(
 				array(
-					'campaign_id' => $campaign_id,
-					'amount' => 50,
+					'campaign_id'   => $campaign_id,
+					'amount'        => 50,
 					'campaign_name' => 'Test Campaign',
 				),
 			),
-			'user' => array(
+			'user'      => array(
 				'first_name' => 'Matthew',
-				'last_name' => 'Murdoch',
-				'email' => 'matthew.murdoch@example.com',
+				'last_name'  => 'Murdoch',
+				'email'      => 'matthew.murdoch@example.com',
 			),
 		);
 
 		return charitable_create_donation( $args );
-
 	}
+
 }
