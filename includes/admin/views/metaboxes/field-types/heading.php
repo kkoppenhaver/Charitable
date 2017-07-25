8d25437c4f6ba9bee67be8d33f795810ce513019
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:31:20 +1000

    Code consistency changes for includes/admin/views/metaboxes/field-types/heading.php

--- includes/admin/views/metaboxes/field-types/heading.php
+++ includes/admin/views/metaboxes/field-types/heading.php
@@ -4,9 +4,9 @@
  *
  * @author      Eric Daams
  * @package     Charitable/Admin Views/Metaboxes
- * @copyright   Copyright (c) 2017, Studio 164a 
+ * @copyright   Copyright (c) 2017, Studio 164a
  * @since       1.2.0
  */
 
 ?>
-<h4 class="charitable-metabox-header"><?php echo $view_args[ 'title' ] ?></h4>
+<h4 class="charitable-metabox-header"><?php echo $view_args['title']; ?></h4>
