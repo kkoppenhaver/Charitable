3b26319e266f8968a586301d57726f0501df319b
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:14:24 +1000

    Code consistency changes for includes/admin/dashboard-widgets/class-charitable-donations-dashboard-widget.php

--- includes/admin/dashboard-widgets/class-charitable-donations-dashboard-widget.php
+++ includes/admin/dashboard-widgets/class-charitable-donations-dashboard-widget.php
@@ -47,7 +47,7 @@ if ( ! class_exists( 'Charitable_Donations_Dashboard_Widget' ) ) :
 		 * @return  void
 		 */
 		public static function display() {
-	?>        
+	?>
 			<p class="hide-if-no-js">
 				<img src="<?php echo charitable()->get_path( 'assets', false ) ?>/images/charitable-loading.gif" width="60" height="60" alt="<?php esc_attr_e( 'Loading&hellip;', 'charitable' ) ?>" />
 			</p>
