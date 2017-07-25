693cb5b7bf479adfb66faf2c7246ce6a24946213
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:36:45 +1000

    Code consistency changes for templates/campaign/finished-notice.php

--- templates/campaign/finished-notice.php
+++ templates/campaign/finished-notice.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying a notice at the top of the campaign 
+ * The template for displaying a notice at the top of the campaign
  * summary to announce how long ago it finished.
  *
  * Override this template by copying it to yourtheme/charitable/campaign/finished-notice.php
@@ -14,15 +14,15 @@
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-$campaign = $view_args[ 'campaign' ];
+$campaign = $view_args['campaign'];
 
 $notice = $campaign->get_finished_notice();
 
 if ( empty( $notice ) ) :
-    return;
+	return;
 endif;
 
 ?>
 <div class="campaign-finished">
-    <?php echo $notice ?>
+    <?php echo $notice; ?>
 </div>
