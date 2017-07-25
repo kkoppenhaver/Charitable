3b38a3e6ee8768568d529a8b05b50cb358629994
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:39:07 +1000

    Code consistency changes for tests/unit-tests/test-currency.php

--- tests/unit-tests/test-currency.php
+++ tests/unit-tests/test-currency.php
@@ -1,14 +1,14 @@
 <?php
 
 class Test_Charitable_Currency_Helper extends Charitable_UnitTestCase {
-	
+
 	private $currency_helper;
 
 	function setUp() {
 		parent::setUp();
-		$this->currency_helper = charitable_get_currency_helper();		
+		$this->currency_helper = charitable_get_currency_helper();
 		$this->set_currency_format( 'left' );
-		$this->set_currency( 'AUD' );		
+		$this->set_currency( 'AUD' );
 		$this->set_decimal_count( 2 );
 		$this->set_decimal_separator( '.' );
 		$this->set_thousands_separator( ',' );
@@ -17,7 +17,7 @@ class Test_Charitable_Currency_Helper extends Charitable_UnitTestCase {
 	/**
 	 * @covers Charitable_Currency::get_monetary_amount()
 	 */
-	function test_get_monetary_amount_with_string() {				
+	function test_get_monetary_amount_with_string() {
 		$this->assertEquals( '&#36;60.00', $this->currency_helper->get_monetary_amount( '60' ) );
 	}
 
@@ -31,17 +31,17 @@ class Test_Charitable_Currency_Helper extends Charitable_UnitTestCase {
 	/**
 	 * @covers Charitable_Currency::sanitize_monetary_amount()
 	 */
-	function test_sanitize_monetary_amount() {		
+	function test_sanitize_monetary_amount() {
 		$this->assertEquals( 60.00, $this->currency_helper->sanitize_monetary_amount( '60' ) );
 	}
 
 	/**
 	 * @covers Charitable_Currency::sanitize_monetary_amount()
 	 */
-	function test_sanitize_monetary_amount_with_decimals() {		
+	function test_sanitize_monetary_amount_with_decimals() {
 		$this->assertEquals( 10.50, $this->currency_helper->sanitize_monetary_amount( '10.50' ) );
 	}
-	
+
 	/**
 	 * @covers Charitable_Currency::sanitize_monetary_amount()
 	 */
@@ -130,19 +130,19 @@ class Test_Charitable_Currency_Helper extends Charitable_UnitTestCase {
 	}
 
 	/**
-     * @expectedException PHPUnit_Framework_Error
-     */
-	function test_sanitize_monetary_amount_exception() {		
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	function test_sanitize_monetary_amount_exception() {
 		$this->assertInstanceOf( 'WP_Error', $this->currency_helper->sanitize_monetary_amount( 10.50 ) );
 	}
-	
+
 	/**
 	 * @covers Charitable_Currency::get_decimals()
-	 */	
+	 */
 	function test_get_decimals() {
-		$this->assertEquals( 2, $this->currency_helper->get_decimals() );	
+		$this->assertEquals( 2, $this->currency_helper->get_decimals() );
 	}
-	
+
 	/**
 	 * @covers Charitable_Currency::get_decimals()
 	 */
@@ -152,58 +152,57 @@ class Test_Charitable_Currency_Helper extends Charitable_UnitTestCase {
 	}
 
 	function test_get_currency_formats() {
-		$this->assertEquals( '%1$s%2$s', $this->currency_helper->get_currency_format() );	
+		$this->assertEquals( '%1$s%2$s', $this->currency_helper->get_currency_format() );
 
-		$this->set_currency_format( 'right-with-space' );		
+		$this->set_currency_format( 'right-with-space' );
 
 		$this->assertEquals( '%2$s&nbsp;%1$s', $this->currency_helper->get_currency_format() );
-		$this->assertEquals( '60.00&nbsp;&#36;', $this->currency_helper->get_monetary_amount('60') );
+		$this->assertEquals( '60.00&nbsp;&#36;', $this->currency_helper->get_monetary_amount( '60' ) );
 	}
 
 	function test_currencies() {
-
 		$this->set_currency_format( 'left' );
 
 		$assertions = array(
-			'AED' => 'د.إ',
-			'BDT' => '&#2547;',
-			'BRL' => '&#82;&#36;',
-			'BGN' => '&#1083;&#1074;.',
-			'AUD' => '&#36;',
-			'USD' => '&#36;',
-			'EUR' => '&euro;',
-			'JPY' => '&yen;',
-			'RUB' => '&#1088;&#1091;&#1073;.',
-			'KRW' => '&#8361;',
-			'TRY' => '&#8378;',
-			'NOK' => '&#107;&#114;',
-			'ZAR' => '&#82;',
-			'CZK' => '&#75;&#269;',
-			'MYR' => '&#82;&#77;',
-			'DKK' => 'kr.',
-			'HUF' => '&#70;&#116;',
-			'IDR' => 'Rp',
-			'INR' => 'Rs.',
-			'ISK' => 'Kr.',
-			'ILS' => '&#8362;',
-			'PHP' => '&#8369;',
-			'PLN' => '&#122;&#322;',
-			'SEK' => '&#107;&#114;',
-			'CHF' => '&#67;&#72;&#70;',
-			'TWD' => '&#78;&#84;&#36;',
-			'THB' => '&#3647;',
-			'GBP' => '&pound;',
-			'RON' => 'lei',
-			'VND' => '&#8363;',
-			'NGN' => '&#8358;',
-			'HRK' => 'Kn',
+			'AED'               => 'د.إ',
+			'BDT'               => '&#2547;',
+			'BRL'               => '&#82;&#36;',
+			'BGN'               => '&#1083;&#1074;.',
+			'AUD'               => '&#36;',
+			'USD'               => '&#36;',
+			'EUR'               => '&euro;',
+			'JPY'               => '&yen;',
+			'RUB'               => '&#1088;&#1091;&#1073;.',
+			'KRW'               => '&#8361;',
+			'TRY'               => '&#8378;',
+			'NOK'               => '&#107;&#114;',
+			'ZAR'               => '&#82;',
+			'CZK'               => '&#75;&#269;',
+			'MYR'               => '&#82;&#77;',
+			'DKK'               => 'kr.',
+			'HUF'               => '&#70;&#116;',
+			'IDR'               => 'Rp',
+			'INR'               => 'Rs.',
+			'ISK'               => 'Kr.',
+			'ILS'               => '&#8362;',
+			'PHP'               => '&#8369;',
+			'PLN'               => '&#122;&#322;',
+			'SEK'               => '&#107;&#114;',
+			'CHF'               => '&#67;&#72;&#70;',
+			'TWD'               => '&#78;&#84;&#36;',
+			'THB'               => '&#3647;',
+			'GBP'               => '&pound;',
+			'RON'               => 'lei',
+			'VND'               => '&#8363;',
+			'NGN'               => '&#8358;',
+			'HRK'               => 'Kn',
 			'AN_UNLISTED_VALUE' => ''
 		);
 
-		foreach ($assertions as $currency_code => $symbol ) {
-			$this->set_currency( $currency_code );	
+		foreach ( $assertions as $currency_code => $symbol ) {
+			$this->set_currency( $currency_code );
 
-			$expected = sprintf( '%1$s%2$s', $symbol, '60.00');
+			$expected = sprintf( '%1$s%2$s', $symbol, '60.00' );
 			$this->assertEquals( $expected, $this->currency_helper->get_monetary_amount( '60' ) );
 		}
 	}
@@ -228,4 +227,5 @@ class Test_Charitable_Currency_Helper extends Charitable_UnitTestCase {
 	private function set_thousands_separator( $separator ) {
 		$this->set_charitable_option( 'thousands_separator', $separator );
 	}
+
 }
