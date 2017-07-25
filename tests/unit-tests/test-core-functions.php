ea2ed908dbbf1274560e7ccd9bdb12fa985f93ac
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:39:05 +1000

    Code consistency changes for tests/unit-tests/test-core-functions.php

--- tests/unit-tests/test-core-functions.php
+++ tests/unit-tests/test-core-functions.php
@@ -9,28 +9,29 @@ class Test_Charitable_Core_Functions extends Charitable_UnitTestCase {
 	function test_charitable() {
 		$this->assertInstanceOf( 'Charitable', charitable() );
 	}
-	
+
 	function test_charitable_get_option() {
 		$this->assertFalse( charitable_get_option( 'nonexistentkey' ) );
 	}
-	
+
 	function test_charitable_get_helper() {
 		$this->assertInstanceOf( 'Charitable_Gateways', charitable_get_helper( 'gateways' ) );
 	}
-	
+
 	function test_charitable_get_notices() {
 		$this->assertInstanceOf( 'Charitable_Notices', charitable_get_notices() );
 	}
-	
+
 	function test_charitable_get_location_helper() {
 		$this->assertInstanceOf( 'Charitable_Locations', charitable_get_location_helper() );
 	}
-	
+
 	function test_charitable_get_session() {
 		$this->assertInstanceOf( 'Charitable_Session', charitable_get_session() );
-	}	
+	}
 
 	function test_charitable_get_deprecated() {
 		$this->assertInstanceOf( 'Charitable_Deprecated', charitable_get_deprecated() );
 	}
+
 }
