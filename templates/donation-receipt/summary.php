f2ec53943b353abbad8048b5b409a4cf1d2f4f4f
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:37:35 +1000

    Code consistency changes for templates/donation-receipt/summary.php

--- templates/donation-receipt/summary.php
+++ templates/donation-receipt/summary.php
@@ -20,12 +20,12 @@ $donation = $view_args['donation'];
 
 ?>
 <dl class="donation-summary">
-	<dt class="donation-id"><?php _e( 'Donation Number:', 'charitable' ) ?></dt>
-	<dd class="donation-summary-value"><?php echo $donation->get_number() ?></dd>
-	<dt class="donation-date"><?php _e( 'Date:', 'charitable' ) ?></dt>
-	<dd class="donation-summary-value"><?php echo $donation->get_date() ?></dd>
-	<dt class="donation-total"> <?php _e( 'Total:', 'charitable' ) ?></dt>
-	<dd class="donation-summary-value"><?php echo charitable_format_money( $donation->get_total_donation_amount() ) ?></dd>
-	<dt class="donation-method"><?php _e( 'Payment Method:', 'charitable' ) ?></dt>
-	<dd class="donation-summary-value"><?php echo $donation->get_gateway_label() ?></dd>
+	<dt class="donation-id"><?php _e( 'Donation Number:', 'charitable' ); ?></dt>
+	<dd class="donation-summary-value"><?php echo $donation->get_number(); ?></dd>
+	<dt class="donation-date"><?php _e( 'Date:', 'charitable' ); ?></dt>
+	<dd class="donation-summary-value"><?php echo $donation->get_date(); ?></dd>
+	<dt class="donation-total"> <?php _e( 'Total:', 'charitable' ); ?></dt>
+	<dd class="donation-summary-value"><?php echo charitable_format_money( $donation->get_total_donation_amount() ); ?></dd>
+	<dt class="donation-method"><?php _e( 'Payment Method:', 'charitable' ); ?></dt>
+	<dd class="donation-summary-value"><?php echo $donation->get_gateway_label(); ?></dd>
 </dl>
