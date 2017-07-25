8e28a147df0ace7ddd3436ca6f4a01f11b7f6640
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:38:56 +1000

    Code consistency changes for tests/unit-tests/test-campaign-donations-db.php

--- tests/unit-tests/test-campaign-donations-db.php
+++ tests/unit-tests/test-campaign-donations-db.php
@@ -1,7 +1,7 @@
 <?php
 
 class Test_Campaign_Donations_DB extends Charitable_UnitTestCase {
-	
+
 	public function setUp() {
 		parent::setUp();
 
@@ -10,30 +10,30 @@ class Test_Campaign_Donations_DB extends Charitable_UnitTestCase {
 
 		$this->donation_1 = Charitable_Donation_Helper::create_donation( array( 'campaigns' => array(
 			array(
-				'campaign_id' 	=> $this->campaign_1, 
-				'campaign_name' => get_the_title( $this->campaign_1 ), 
-				'amount'		=> 10
+				'campaign_id'   => $this->campaign_1,
+				'campaign_name' => get_the_title( $this->campaign_1 ),
+				'amount'        => 10
 			)
 		) ) );
 
 		$this->donation_2 = Charitable_Donation_Helper::create_donation( array( 'campaigns' => array(
 			array(
-				'campaign_id' 	=> $this->campaign_2, 
-				'campaign_name' => get_the_title( $this->campaign_2 ), 
-				'amount'		=> 10
+				'campaign_id'   => $this->campaign_2,
+				'campaign_name' => get_the_title( $this->campaign_2 ),
+				'amount'        => 10
 			)
 		) ) );
 
 		$this->donation_3 = Charitable_Donation_Helper::create_donation( array( 'campaigns' => array(
 			array(
-				'campaign_id' 	=> $this->campaign_1, 
-				'campaign_name' => get_the_title( $this->campaign_1 ), 
-				'amount'		=> 30
-			), 
+				'campaign_id'   => $this->campaign_1,
+				'campaign_name' => get_the_title( $this->campaign_1 ),
+				'amount'        => 30
+			),
 			array(
-				'campaign_id' 	=> $this->campaign_2, 
-				'campaign_name' => get_the_title( $this->campaign_2 ), 
-				'amount'		=> 40
+				'campaign_id'   => $this->campaign_2,
+				'campaign_name' => get_the_title( $this->campaign_2 ),
+				'amount'        => 40
 			)
 		) ) );
 	}
@@ -44,24 +44,24 @@ class Test_Campaign_Donations_DB extends Charitable_UnitTestCase {
 
 	public function test_insert() {
 		$args = array(
-			'donation_id'	=> 0,
-			'campaign_id'	=> $this->campaign_1,
-			'amount'		=> 10
+			'donation_id' => 0,
+			'campaign_id' => $this->campaign_1,
+			'amount'      => 10
 		);
 
-		$campaign_donation_id = charitable_get_table('campaign_donations')->insert( $args );
+		$campaign_donation_id = charitable_get_table( 'campaign_donations' )->insert( $args );
 
 		$this->assertGreaterThan( 0, $campaign_donation_id );
 	}
 
 	public function test_count_all() {
-		$this->assertEquals( 4, charitable_get_table('campaign_donations')->count_all() );
+		$this->assertEquals( 4, charitable_get_table( 'campaign_donations' )->count_all() );
 	}
 
 	/**
 	 * @depends test_count_all
 	 */
 	public function test_get_donation_records() {
-
 	}
+
 }
