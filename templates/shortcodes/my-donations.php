8b4d76c2654dd980cb879f022065522b8ae686b3
ericnicolaas <eric@ericnicolaas.com>
2017-07-25 17:38:31 +1000

    Code consistency changes for templates/shortcodes/my-donations.php

--- templates/shortcodes/my-donations.php
+++ templates/shortcodes/my-donations.php
@@ -22,28 +22,28 @@ do_action( 'charitable_my_donations_before', $donations );
 
 if ( empty( $donations ) ) : ?>
 
-	<p><?php _e( 'You have not made any donations yet.', 'charitable' ) ?></p>
+	<p><?php _e( 'You have not made any donations yet.', 'charitable' ); ?></p>
 
 <?php else : ?>
 
 	<table class="charitable-creator-donations">
 		<thead>
 			<tr>
-				<th scope="col"><?php _e( 'Date', 'charitable' ) ?></th>
-				<th scope="col"><?php _e( 'Campaign', 'charitable' ) ?></th>
-				<th scope="col"><?php _e( 'Amount', 'charitable' ) ?></th>
-				<th scope="col"><?php _e( 'Receipt', 'charitable' ) ?></th>
+				<th scope="col"><?php _e( 'Date', 'charitable' ); ?></th>
+				<th scope="col"><?php _e( 'Campaign', 'charitable' ); ?></th>
+				<th scope="col"><?php _e( 'Amount', 'charitable' ); ?></th>
+				<th scope="col"><?php _e( 'Receipt', 'charitable' ); ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php foreach ( $donations as $donation ) : ?>
 			<tr>
-				<td><?php echo mysql2date( 'F j, Y', get_post_field( 'post_date', $donation->ID ) )?></td>
-				<td><?php echo $donation->campaigns ?></td>
-				<td><?php echo charitable_format_money( $donation->amount ) ?></td>
-				<td><a href="<?php echo esc_url( charitable_get_permalink( 'donation_receipt_page', array( 'donation_id' => $donation->ID ) ) ) ?>"><?php _e( 'View Receipt', 'charitable' ) ?></a></td>
+				<td><?php echo mysql2date( 'F j, Y', get_post_field( 'post_date', $donation->ID ) ); ?></td>
+				<td><?php echo $donation->campaigns; ?></td>
+				<td><?php echo charitable_format_money( $donation->amount ); ?></td>
+				<td><a href="<?php echo esc_url( charitable_get_permalink( 'donation_receipt_page', array( 'donation_id' => $donation->ID ) ) ); ?>"><?php _e( 'View Receipt', 'charitable' ); ?></a></td>
 			</tr>
-			<?php endforeach ?>
+			<?php endforeach; ?>
 		</tbody>
 	</table>
 
