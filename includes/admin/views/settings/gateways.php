5e7f7961010f2ccc4781a98f8370ca2a544327b4
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 17:56:19 +1000

    Code consistency changes for includes/admin/views/settings/gateways.php

--- includes/admin/views/settings/gateways.php
+++ includes/admin/views/settings/gateways.php
@@ -7,9 +7,9 @@
  * @since   1.0.0
  */
 
-$helper     = charitable_get_helper( 'gateways' );
-$gateways   = $helper->get_available_gateways();
-$default    = $helper->get_default_gateway();
+$helper   = charitable_get_helper( 'gateways' );
+$gateways = $helper->get_available_gateways();
+$default  = $helper->get_default_gateway();
 
 if ( count( $gateways ) ) :
 
@@ -31,16 +31,16 @@ if ( count( $gateways ) ) :
 
 		?>
 		<div class="charitable-settings-object charitable-gateway cf">
-			<h4><?php echo $gateway->get_name() ?></h4>
+			<h4><?php echo $gateway->get_name(); ?></h4>
 			<?php if ( $gateway->get_gateway_id() == $default ) : ?>
 
-				<span class="default-gateway"><?php _e( 'Default gateway', 'charitable' ) ?></span>
+				<span class="default-gateway"><?php _e( 'Default gateway', 'charitable' ); ?></span>
 
 			<?php elseif ( $is_active ) : ?>
 
-				<a href="<?php echo $make_default_url ?>" class="make-default-gateway"><?php _e( 'Make default gateway', 'charitable' ) ?></a>
+				<a href="<?php echo $make_default_url; ?>" class="make-default-gateway"><?php _e( 'Make default gateway', 'charitable' ); ?></a>
 
-			<?php endif ?>
+			<?php endif; ?>
 			<span class="actions">
 				<?php if ( $is_active ) :
 					$settings_url = esc_url( add_query_arg( array(
@@ -48,22 +48,22 @@ if ( count( $gateways ) ) :
 					), admin_url( 'admin.php?page=charitable-settings&tab=gateways' ) ) );
 					?>
 
-					<a href="<?php echo $settings_url ?>" class="button button-primary"><?php _e( 'Gateway Settings', 'charitable' ) ?></a>
+					<a href="<?php echo $settings_url; ?>" class="button button-primary"><?php _e( 'Gateway Settings', 'charitable' ); ?></a>
 
 				<?php endif;
 
 if ( $is_active ) : ?>
 
-					<a href="<?php echo $action_url ?>" class="button"><?php _e( 'Disable Gateway', 'charitable' ) ?></a>
+					<a href="<?php echo $action_url; ?>" class="button"><?php _e( 'Disable Gateway', 'charitable' ); ?></a>
 
 				<?php else : ?>
 
-					<a href="<?php echo $action_url ?>" class="button"><?php _e( 'Enable Gateway', 'charitable' ) ?></a>
+					<a href="<?php echo $action_url; ?>" class="button"><?php _e( 'Enable Gateway', 'charitable' ); ?></a>
 
-				<?php endif ?>
+				<?php endif; ?>
 			</span>
 		</div>
-	<?php endforeach ?>
+	<?php endforeach; ?>
 <?php else : ?>
-	<?php _e( 'There are no gateways available in your system.', 'charitable' ) ?>
-<?php endif ?>
+	<?php _e( 'There are no gateways available in your system.', 'charitable' ); ?>
+<?php endif;
