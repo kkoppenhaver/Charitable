64c0e992e2a4ec2e64a44a81d48edfcd0f318b1c
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 17:57:26 +1000

    Code consistency changes for includes/class-charitable-upgrade.php

--- includes/class-charitable-upgrade.php
+++ includes/class-charitable-upgrade.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * This provides backwards compatibility for any extensions that 
+ * This provides backwards compatibility for any extensions that
  * attempt to load the Charitable_Upgrade class from here.
  *
  * @deprecated
  */
 
 if ( class_exists( 'Charitable_Upgrade' ) ) {
-    return;
+	return;
 }
 
 require_once( charitable()->get_path( 'admin' ) . 'upgrades/class-charitable-upgrade.php' );
