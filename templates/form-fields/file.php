b83c1197d7427f09cd967f9185acdb826fb6d899
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 18:02:50 +1000

    Code consistency changes for templates/form-fields/file.php

--- templates/form-fields/file.php
+++ templates/form-fields/file.php
@@ -12,16 +12,16 @@ if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
 	return;
 }
 
-$form 	 = $view_args['form'];
-$field 	 = $view_args['field'];
+$form    = $view_args['form'];
+$field   = $view_args['field'];
 $classes = $view_args['classes'];
-$value	 = isset( $field['value'] ) ? $field['value'] : '';
+$value   = isset( $field['value'] ) ? $field['value'] : '';
 ?>
-<div id="charitable_field_<?php echo $field['key'] ?>" class="<?php echo $classes ?>">	
+<div id="charitable_field_<?php echo $field['key']; ?>" class="<?php echo $classes; ?>">
 	<?php if ( isset( $field['label'] ) ) : ?>
-		<label for="charitable_field_<?php echo $field['key'] ?>_element">
-			<?php echo $field['label'] ?>			
+		<label for="charitable_field_<?php echo $field['key']; ?>_element">
+			<?php echo $field['label']; ?>
 		</label>
-	<?php endif ?>
-	<input type="file" name="<?php echo $field['key'] ?>" id="charitable_field_<?php echo $field['key'] ?>_element" value="<?php echo $value ?>" <?php echo charitable_get_arbitrary_attributes( $field ) ?>/>
+	<?php endif; ?>
+	<input type="file" name="<?php echo $field['key']; ?>" id="charitable_field_<?php echo $field['key']; ?>_element" value="<?php echo $value; ?>" <?php echo charitable_get_arbitrary_attributes( $field ); ?>/>
 </div>
