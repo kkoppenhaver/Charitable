4e7365e3c5f7c314337e84254348a9574c6cdfcd
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 17:56:04 +1000

    Code consistency changes for includes/admin/views/metaboxes/field-types/textarea.php

--- includes/admin/views/metaboxes/field-types/textarea.php
+++ includes/admin/views/metaboxes/field-types/textarea.php
@@ -14,18 +14,18 @@ if ( ! isset( $view_args['meta_key'] ) ) {
 	return;
 }
 
-$key = $view_args['meta_key'];
+$key         = $view_args['meta_key'];
 $custom_keys = get_post_custom_keys( $post->ID );
-$value = $custom_keys && in_array( $key, $custom_keys ) ? get_post_meta( $post->ID, $key, true ) : $view_args['default'];
+$value       = $custom_keys && in_array( $key, $custom_keys ) ? get_post_meta( $post->ID, $key, true ) : $view_args['default'];
 
-$id = ltrim( $key, '_' );
-$id = str_replace( '_', '-', $id );
+$id         = ltrim( $key, '_' );
+$id         = str_replace( '_', '-', $id );
 $wrapper_id = 'charitable-' . $id . '-wrap';
 
 ?>
-<div id="<?php echo $wrapper_id ?>" class="charitable-metabox-wrap charitable-text-field-wrap">
+<div id="<?php echo $wrapper_id; ?>" class="charitable-metabox-wrap charitable-text-field-wrap">
 	<?php if ( isset( $view_args['label'] ) ) : ?>
-		<label for="<?php echo $id ?>"><?php echo $view_args['label']  ?></label>
-	<?php endif ?>
-	<textarea id="<?php echo $id ?>" name="<?php echo esc_attr( $key ) ?>"><?php echo esc_textarea( $value ) ?></textarea>
+		<label for="<?php echo $id; ?>"><?php echo $view_args['label'];  ?></label>
+	<?php endif; ?>
+	<textarea id="<?php echo $id; ?>" name="<?php echo esc_attr( $key ); ?>"><?php echo esc_textarea( $value ); ?></textarea>
 </div>
