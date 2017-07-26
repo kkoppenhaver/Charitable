33568537fa2d0fe84bc869e9570cb16fa876204a
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 18:03:00 +1000

    Code consistency changes for templates/form-fields/paragraph.php

--- templates/form-fields/paragraph.php
+++ templates/form-fields/paragraph.php
@@ -7,18 +7,18 @@
  * @version 1.0.0
  */
 
-if ( ! isset( $view_args[ 'form' ] ) || ! isset( $view_args[ 'field' ] ) ) {
+if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
 	return;
 }
 
-$form 			= $view_args[ 'form' ];
-$field 			= $view_args[ 'field' ];
-$classes        = $view_args[ 'classes' ];
+$form    = $view_args['form'];
+$field   = $view_args['field'];
+$classes = $view_args['classes'];
 
-if ( ! isset( $field[ 'content' ] ) ) {
+if ( ! isset( $field['content'] ) ) {
 	return;
 }
 ?>
-<p class="<?php echo $classes ?>">
-	<?php echo $field[ 'content' ] ?>
+<p class="<?php echo $classes; ?>">
+	<?php echo $field['content']; ?>
 </p>
