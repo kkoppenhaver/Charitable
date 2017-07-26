f560e5221d6d7540f1520a9b080fcd7d9df54524
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 18:02:06 +1000

    Code consistency changes for templates/donation-form/donor-fields/login-form.php

--- templates/donation-form/donor-fields/login-form.php
+++ templates/donation-form/donor-fields/login-form.php
@@ -13,13 +13,13 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 ?>
 <p class="login-prompt">
-    <a href="#" data-charitable-toggle="charitable-donation-login-form"><?php _e( 'Donated before? Log in before continuing.', 'charitable' ) ?></a>
+    <a href="#" data-charitable-toggle="charitable-donation-login-form"><?php _e( 'Donated before? Log in before continuing.', 'charitable' ); ?></a>
 </p>
 <div id="charitable-donation-login-form" class="charitable-login-form charitable-form">
-    <p><?php _e( 'If you have donated before, please enter your details below to login. If this is your first time, proceed to the donation form.', 'charitable' ) ?></p>
+    <p><?php _e( 'If you have donated before, please enter your details below to login. If this is your first time, proceed to the donation form.', 'charitable' ); ?></p>
     <?php wp_login_form(
-        array( 
-            'label_username' => apply_filters( 'charitable_donor_usernames', false ) ? __( 'Username', 'charitable' ) : __( 'Email', 'charitable' )
-        ) 
-    ) ?>
+		array(
+			'label_username' => apply_filters( 'charitable_donor_usernames', false ) ? __( 'Username', 'charitable' ) : __( 'Email', 'charitable' )
+		)
+	); ?>
 </div>
