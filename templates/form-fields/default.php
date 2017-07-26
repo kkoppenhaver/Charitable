7e63b5ca3a907caf9128fb1a444cbeed517657f5
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 18:02:41 +1000

    Code consistency changes for templates/form-fields/default.php

--- templates/form-fields/default.php
+++ templates/form-fields/default.php
@@ -12,25 +12,25 @@ if ( ! isset( $view_args['form'] ) || ! isset( $view_args['field'] ) ) {
 	return;
 }
 
-$form           = $view_args['form'];
-$field          = $view_args['field'];
-$classes        = esc_attr( $view_args['classes'] );
-$field_type     = isset( $field['type'] ) ? $field['type'] : 'text';
-$is_required    = isset( $field['required'] ) ? $field['required'] : false;
-$value          = isset( $field['value'] ) ? $field['value'] : '';
+$form        = $view_args['form'];
+$field       = $view_args['field'];
+$classes     = esc_attr( $view_args['classes'] );
+$field_type  = isset( $field['type'] ) ? $field['type'] : 'text';
+$is_required = isset( $field['required'] ) ? $field['required'] : false;
+$value       = isset( $field['value'] ) ? $field['value'] : '';
 
 ?>
-<div id="charitable_field_<?php echo $field['key'] ?>" class="<?php echo $classes ?>">
+<div id="charitable_field_<?php echo $field['key']; ?>" class="<?php echo $classes; ?>">
 	<?php if ( isset( $field['label'] ) ) : ?>
-		<label for="charitable_field_<?php echo $field['key'] ?>_element">
-			<?php echo $field['label'] ?>
+		<label for="charitable_field_<?php echo $field['key']; ?>_element">
+			<?php echo $field['label']; ?>
 			<?php if ( $is_required ) : ?>
 				<abbr class="required" title="required">*</abbr>
-			<?php endif ?>
+			<?php endif; ?>
 		</label>
-	<?php endif ?>
+	<?php endif; ?>
 	<?php if ( isset( $field['help'] ) ) : ?>
-		<p class="charitable-field-help"><?php echo $field['help'] ?></p>
-	<?php endif ?>
-	<input type="<?php echo esc_attr( $field_type ) ?>" name="<?php echo $field['key'] ?>" id="charitable_field_<?php echo $field['key'] ?>_element" value="<?php echo esc_attr( stripslashes( $value ) ) ?>" <?php echo charitable_get_arbitrary_attributes( $field ) ?>/>
+		<p class="charitable-field-help"><?php echo $field['help']; ?></p>
+	<?php endif; ?>
+	<input type="<?php echo esc_attr( $field_type ); ?>" name="<?php echo $field['key']; ?>" id="charitable_field_<?php echo $field['key']; ?>_element" value="<?php echo esc_attr( stripslashes( $value ) ); ?>" <?php echo charitable_get_arbitrary_attributes( $field ); ?>/>
 </div>
