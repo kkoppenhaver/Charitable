b5224bc4e90779e8287061118cbdd3f30bb37d0b
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 18:03:02 +1000

    Code consistency changes for templates/form-fields/picture-preview.php

--- templates/form-fields/picture-preview.php
+++ templates/form-fields/picture-preview.php
@@ -12,23 +12,23 @@ if ( ! isset( $view_args['image'] ) || ! isset( $view_args['field'] ) ) {
 	return;
 }
 
-$image = $view_args['image'];
-$field = $view_args['field'];
-$size = isset( $field['size'] ) ? $field['size'] : 'thumbnail';
+$image    = $view_args['image'];
+$field    = $view_args['field'];
+$size     = isset( $field['size'] ) ? $field['size'] : 'thumbnail';
 $multiple = isset( $field['max_uploads'] ) && $field['max_uploads'] > 1 ? '[]' : '';
-$is_src = strpos( $image, 'img' ) !== false;
+$is_src   = strpos( $image, 'img' ) !== false;
 
 if ( is_numeric( $size ) ) {
 	$size = array( $size, $size );
 }
 
 ?>
-<li <?php if ( ! $is_src ) : ?>data-attachment-id="<?php echo $image ?>"<?php endif ?>>
-	<a href="#" class="remove-image button"><?php _e( 'Remove', 'charitable' ) ?></a>
+<li <?php if ( ! $is_src ) : ?>data-attachment-id="<?php echo $image; ?>"<?php endif; ?>>
+	<a href="#" class="remove-image button"><?php _e( 'Remove', 'charitable' ); ?></a>
 	<?php if ( $is_src ) :
 		echo $image;
 	else : ?>
-		<input type="hidden" name="<?php echo $field['key'] . $multiple ?>" id="charitable_field_<?php echo $field['key'] ?>_element" value="<?php echo $image ?>" />
-		<?php echo wp_get_attachment_image( $image, $size ) ?>
-	<?php endif ?>
+		<input type="hidden" name="<?php echo $field['key'] . $multiple; ?>" id="charitable_field_<?php echo $field['key']; ?>_element" value="<?php echo $image; ?>" />
+		<?php echo wp_get_attachment_image( $image, $size ); ?>
+	<?php endif; ?>
 </li>
