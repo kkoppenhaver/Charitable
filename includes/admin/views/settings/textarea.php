34e6e84cc103bf514a55acef2c9b9e8b0da39dc8
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 17:56:46 +1000

    Code consistency changes for includes/admin/views/settings/textarea.php

--- includes/admin/views/settings/textarea.php
+++ includes/admin/views/settings/textarea.php
@@ -17,14 +17,14 @@ $rows = isset( $field['rows'] ) ? $field['rows'] : 4;
 ?>
 
 <textarea
-	id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args['key'] ) ) ?>" 
-	name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ) ?>"
-	class="<?php echo esc_attr( $view_args['classes'] ) ?>"
-	rows="<?php echo absint( $rows ) ?>"
-	<?php echo charitable_get_arbitrary_attributes( $view_args ) ?>><?php echo esc_textarea( $value ) ?></textarea>
+	id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args['key'] ) ); ?>"
+	name="<?php printf( 'charitable_settings[%s]', $view_args['name'] ); ?>"
+	class="<?php echo esc_attr( $view_args['classes'] ); ?>"
+	rows="<?php echo absint( $rows ); ?>"
+	<?php echo charitable_get_arbitrary_attributes( $view_args ); ?>><?php echo esc_textarea( $value ); ?></textarea>
 
 <?php if ( isset( $view_args['help'] ) ) : ?>
 
-	<div class="charitable-help"><?php echo $view_args['help']  ?></div>
+	<div class="charitable-help"><?php echo $view_args['help'];  ?></div>
 
 <?php endif;
