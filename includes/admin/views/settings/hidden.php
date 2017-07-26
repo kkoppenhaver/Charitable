2658243f780c0d5ec31432cdc34371e35cc2774c
ericnicolaas <eric@ericnicolaas.com>
2017-07-26 17:56:23 +1000

    Code consistency changes for includes/admin/views/settings/hidden.php

--- includes/admin/views/settings/hidden.php
+++ includes/admin/views/settings/hidden.php
@@ -7,10 +7,10 @@
  * @since   1.0.0
  */
 
-$value = $view_args[ 'value' ];
+$value = $view_args['value'];
 
 ?>
-<input type="hidden"  
-    id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args[ 'key' ] ) ) ?>" 
-    name="<?php printf( 'charitable_settings[%s]', $view_args[ 'name' ] ) ?>"
-    value="<?php echo esc_attr( $value ) ?>"  />
+<input type="hidden"
+    id="<?php printf( 'charitable_settings_%s', implode( '_', $view_args[ 'key' ] ) ); ?>"
+    name="<?php printf( 'charitable_settings[%s]', $view_args[ 'name' ] ); ?>"
+    value="<?php echo esc_attr( $value ); ?>"  />
